54089944b498dd4273505d9f55cb20956fa4def1
Lorenzo Gracia <lgracia@cimne.upc.edu>
2017-03-01 16:31:56 +0100

    Cleaning projectparameters and adding new function for streamlines

--- kratos.gid/apps/Dam/write/writeProjectParameters.tcl
+++ kratos.gid/apps/Dam/write/writeProjectParameters.tcl
@@ -16,6 +16,7 @@ proc Dam::write::getParametersDict { } {
     dict set problemDataDict start_time [write::getValue DamTimeParameters StartTime]
     dict set problemDataDict end_time [write::getValue DamTimeParameters EndTime]
     dict set problemDataDict time_step [write::getValue DamTimeParameters DeltaTime]
+    dict set problemDataDict streamlines_utility [Dam::write::StremalinesUtility]
     set paralleltype [write::getValue ParallelType]
     dict set generalDataDict "parallel_type" $paralleltype
     if {$paralleltype eq "OpenMP"} {
@@ -219,75 +220,6 @@ proc Dam::write::getParametersDict { } {
     return $projectParametersDict
 
 }
-    #~ set damTypeofProblem [write::getValue DamTypeofProblem]
-    #~ if {$damTypeofProblem eq "Thermo-Mechanical"} {
-        #~ set diffusionSolverSettingsDict [dict create]
-        #~ set variablesDict [dict create] 
-        #~ dict set variablesDict unknown_variable "KratosMultiphysics.TEMPERATURE"
-        #~ dict set variablesDict diffusion_variable "KratosMultiphysics.CONDUCTIVITY"
-        #~ dict set variablesDict specific_heat_variable "KratosMultiphysics.SPECIFIC_HEAT"
-        #~ dict set variablesDict density_variable "KratosMultiphysics.DENSITY"
-        #~ dict set diffusionSolverSettingsDict variables $variablesDict
-        #~ set thermal_sol_strat [write::getValue DamSolStratTherm]
-        #~ dict set diffusionSolverSettingsDict temporal_scheme [write::getValue DamMechanicalSchemeTherm]
-        #~ dict set diffusionSolverSettingsDict reference_temperature [write::getValue DamReferenceTemperature]
-        
-    #~ ### Add section to document
-    #~ dict set projectParametersDict diffusion_settings $diffusionSolverSettingsDict
-    #~ }     
-       
-    ### Mechanical Settings
-    #~ set mechanicalSolverSettingsDict [dict create]
-    #~ dict set mechanicalSolverSettingsDict solver_type "dam_new_mechanical_solver"
-    #~ set modelDict [dict create]
-    #~ dict set modelDict input_type "mdpa"
-    #~ dict set modelDict input_filename $model_name
-    #~ dict set mechanicalSolverSettingsDict model_import_settings $modelDict
-    #~ dict set mechanicalSolverSettingsDict solution_type [write::getValue DamSoluType]
-    #~ dict set mechanicalSolverSettingsDict analysis_type [write::getValue DamAnalysisType]
-    #~ dict set mechanicalSolverSettingsDict strategy_type [write::getValue DamSolStrat]
-    #~ dict set mechanicalSolverSettingsDict scheme_type [write::getValue DamScheme]
-    #~ set mechanicalSolverSettingsDict [dict merge $mechanicalSolverSettingsDict [write::getSolutionStrategyParametersDict] ]
-    #~ dict set mechanicalSolverSettingsDict type_of_builder [write::getValue DamTypeofbuilder]
-    #~ dict set mechanicalSolverSettingsDict type_of_solver [write::getValue DamTypeofsolver]
-    #~ set typeofsolver [write::getValue DamTypeofsolver]
-    #~ if {$typeofsolver eq "Direct"} {
-        #~ dict set mechanicalSolverSettingsDict solver_class [write::getValue DamDirectsolver]
-    #~ } elseif {$typeofsolver eq "Iterative"} {
-        #~ dict set mechanicalSolverSettingsDict solver_class [write::getValue DamIterativesolver]
-    #~ }
-    #~ ### Add section to document
-    #~ dict set projectParametersDict mechanical_settings $mechanicalSolverSettingsDict
-    
-    ### Boundary conditions processes
-    #~ set body_part_list [list ]
-    #~ set joint_part_list [list ]
-    #~ set mat_dict [write::getMatDict]
-    #~ foreach part_name [dict keys $mat_dict] {
-        #~ if {[[Model::getElement [dict get $mat_dict $part_name Element]] getAttribute "ElementType"] eq "Joint"} {
-            #~ lappend joint_part_list [write::getMeshId Parts $part_name]
-        #~ } {
-            #~ lappend body_part_list [write::getMeshId Parts $part_name]
-        #~ }
-    #~ }
-    #~ dict set projectParametersDict problem_domain_sub_model_part_list [write::getSubModelPartNames "DamParts"]
-    #~ dict set projectParametersDict problem_domain_body_sub_model_part_list $body_part_list
-    #~ dict set projectParametersDict problem_domain_joint_sub_model_part_list $joint_part_list
-    #~ dict set projectParametersDict processes_sub_model_part_list [write::getSubModelPartNames "DamNodalConditions" "DamLoads"]
-    #~ set nodal_process_list [write::getConditionsParametersDict DamNodalConditions "Nodal"]
-    #~ set load_process_list [write::getConditionsParametersDict DamLoads ]
-    
-    #~ dict set projectParametersDict nodal_processes_sub_model_part_list [Dam::write::ChangeFileNameforTableid $nodal_process_list]
-    #~ dict set projectParametersDict load_processes_sub_model_part_list [Dam::write::ChangeFileNameforTableid $load_process_list]
-    #~ set strategytype [write::getValue DamSolStrat]
-    #~ if {$strategytype eq "Arc-length"} {
-        #~ dict set projectParametersDict loads_sub_model_part_list [write::getSubModelPartNames "DamLoads"]
-        #~ dict set projectParametersDict loads_variable_list [Dam::write::getVariableParametersDict DamLoads]
-    #~ }
-    #~ ### GiD output configuration
-    #~ dict set projectParametersDict output_configuration [write::GetDefaultOutputDict]
-    #~ return $projectParametersDict
-
 
 proc Dam::write::DefinitionDomains { } {
     
@@ -345,34 +277,15 @@ proc Dam::write::writeParametersEvent { } {
     write::WriteJSON $projectParametersDict
 }
 
-proc Dam::write::GetDefaultOutputDict {} {
-    set outputDict [dict create]
-    set resultDict [dict create]
-    
-    set GiDPostDict [dict create]
-    dict set GiDPostDict GiDPostMode                [write::getValue Results GiDPostMode]
-    dict set GiDPostDict WriteDeformedMeshFlag      [write::getValue Results GiDWriteMeshFlag]
-    dict set GiDPostDict WriteConditionsFlag        [write::getValue Results GiDWriteConditionsFlag]
-    dict set GiDPostDict MultiFileFlag              [write::getValue Results GiDMultiFileFlag]
-    dict set resultDict gidpost_flags $GiDPostDict
-    
-    dict set resultDict file_label [write::getValue Results FileLabel] 
-    dict set resultDict output_control_type [write::getValue Results OutputControlType] 
-    set output_control [write::getValue DamOutputControlType]
-    if {$output_control eq "step"} {
-        dict set resultDict output_frequency [write::getValue Results OutputDeltaStep]
-    } else {
-        dict set resultDict output_frequency [write::getValue Results OutputDeltaTime]
+proc Dam::write::StremalinesUtility {} {
+
+    set nodalList [write::GetResultsList NodalResults]
+    if {[lsearch $nodalList Vi_POSITIVE] >= 0 || [lsearch $nodalList Viii_POSITIVE] >= 0} {
+        set streamlines true
+    } {
+        set streamlines false
     }
-    dict set resultDict body_output [write::getValue Results BodyOutput] 
-    dict set resultDict node_output [write::getValue Results NodeOutput] 
-    dict set resultDict skin_output [write::getValue Results SkinOutput]
-    dict set resultDict plane_output [write::GetResultsList Results CutPlanes]   
-    dict set resultDict nodal_results [write::GetResultsList NodalResults]
-    dict set resultDict gauss_point_results [write::GetResultsList ElementResults]
-    
-    dict set outputDict "result_file_configuration" $resultDict
-    return $outputDict
+    return $streamlines
 }
 
  # appid Dam solStratUN DamSolStrat problem_base_UN DamMechanicalData
