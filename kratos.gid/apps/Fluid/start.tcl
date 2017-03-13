8529134beb0f335b97d78f9e74ec9a940dd4ee40
Javi Garate <fjgarate@cimne.upc.edu>
2017-03-13 20:27:31 +0100

    #19 Potential fluid first steps

--- kratos.gid/apps/Fluid/start.tcl
+++ kratos.gid/apps/Fluid/start.tcl
@@ -81,7 +81,7 @@ proc ::Fluid::FluidAppSelectorWindow { } {
 
         ttk::frame $w.information  -relief ridge
         set i 0
-        set apps [list Fluid EmbeddedFluid]
+        set apps [list Fluid EmbeddedFluid PotentialFluid]
         foreach app $apps {
             set img [::apps::getImgFrom $app]
             set app_publicname [[::apps::getAppById $app] getPublicName]
