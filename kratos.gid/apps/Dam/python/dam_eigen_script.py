40d328650e059740ee66ebbed9b432977ee18f1f
Lorenzo Gracia <lgracia@cimne.upc.edu>
2017-03-15 16:56:18 +0100

    Cleaning new dam eigen script

--- kratos.gid/apps/Dam/python/dam_eigen_script.py
+++ kratos.gid/apps/Dam/python/dam_eigen_script.py
@@ -42,36 +42,12 @@ problem_name = ProjectParameters["problem_data"]["problem_name"].GetString()
 domain_size = ProjectParameters["problem_data"]["domain_size"].GetInt()
 echo_level = ProjectParameters["solver_settings"]["echo_level"].GetInt()
 buffer_size = ProjectParameters["solver_settings"]["buffer_size"].GetInt()
-delta_time = ProjectParameters["problem_data"]["time_step"].GetDouble()
-end_time = ProjectParameters["problem_data"]["end_time"].GetDouble()
-time = ProjectParameters["problem_data"]["start_time"].GetDouble()
-tol = delta_time*1.0e-10
-time_scale = ProjectParameters["problem_data"]["time_scale"].GetString()
-# Time Units Converter
-if(time_scale=="Months"):               # Factor to pass from months to seconds
-    time_unit_converter = 2592000.0
-elif(time_scale=="Days"):               # Factor to pass from days to seconds
-    time_unit_converter = 86400.0
-elif(time_scale=="Hours"):              # Factor to pass from hours to seconds
-    time_unit_converter = 3600.0
-else:                                       # No changes
-    time_unit_converter = 1.0
-
-# Update time variables
-delta_time = delta_time * time_unit_converter
-end_time = end_time * time_unit_converter
-time = time * time_unit_converter
-tol = tol * time_unit_converter
 
 ## Model part ------------------------------------------------------------------------------------------------
 
 # Defining the model part
 
 main_model_part = KratosMultiphysics.ModelPart(ProjectParameters["problem_data"]["model_part_name"].GetString())
-main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, domain_size)
-main_model_part.ProcessInfo.SetValue(KratosMultiphysics.TIME, time)
-main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DELTA_TIME, delta_time)
-main_model_part.ProcessInfo.SetValue(KratosPoro.TIME_UNIT_CONVERTER, time_unit_converter)
 
 # Construct the solver (main setting methods are located in the solver_module)
 solver_module = __import__(ProjectParameters["solver_settings"]["solver_type"].GetString())
@@ -102,14 +78,12 @@ if(echo_level > 1):
     for properties in main_model_part.Properties:
         print(properties)
 
-
 ## Initialize ------------------------------------------------------------------------------------------------
 
 # Construct the processes to be applied
 import process_factory
 list_of_processes =  process_factory.KratosProcessFactory(DamModel).ConstructListOfProcesses( ProjectParameters["constraints_process_list"] )
 list_of_processes += process_factory.KratosProcessFactory(DamModel).ConstructListOfProcesses( ProjectParameters["loads_process_list"] )
-list_of_processes += process_factory.KratosProcessFactory(DamModel).ConstructListOfProcesses( ProjectParameters["post_eigen_values"] )
 
 # Print list of constructed processes
 if(echo_level>1):
@@ -120,15 +94,22 @@ if(echo_level>1):
 for process in list_of_processes:
     process.ExecuteInitialize()
 
-# Set TIME and DELTA_TIME and fill the previous steps of the buffer with the initial conditions
-time = time - (buffer_size-1)*delta_time
-main_model_part.ProcessInfo.SetValue(KratosMultiphysics.TIME, time)
-for step in range(buffer_size-1):
-    time = time + delta_time
-    main_model_part.CloneTimeStep(time)
-
 # Initialize GiD I/O
 computing_model_part = solver.GetComputingModelPart()
+output_settings = ProjectParameters["output_configuration"]
+if parallel_type == "OpenMP":
+    import poromechanics_cleaning_utility
+    poromechanics_cleaning_utility.CleanPreviousFiles(problem_path) # Clean previous post files
+    from gid_output_process import GiDOutputProcess
+    gid_output = GiDOutputProcess(computing_model_part,
+                                  problem_name,
+                                  output_settings)
+else:
+    from gid_output_process_mpi import GiDOutputProcessMPI
+    gid_output = GiDOutputProcessMPI(computing_model_part,
+                                     problem_name,
+                                     output_settings)
+gid_output.ExecuteInitialize()
 
 #Initialize the solver
 solver.Initialize()
@@ -137,32 +118,44 @@ solver.Initialize()
 for process in list_of_processes:
     process.ExecuteBeforeSolutionLoop()
 
-## Temporal loop ---------------------------------------------------------------------------------------------
+## Set results when they are written in a single file
+gid_output.ExecuteBeforeSolutionLoop()
 
-while( (time+tol) <= end_time ):
+## It is just need one step ---------------------------------------------------------------------------------------------
 
-    # Update temporal variables
-    delta_time = main_model_part.ProcessInfo[KratosMultiphysics.DELTA_TIME]
-    time = time + delta_time
-    main_model_part.CloneTimeStep(time)
+# Update imposed conditions
+for process in list_of_processes:
+    process.ExecuteInitializeSolutionStep()
 
-    # Update imposed conditions
-    for process in list_of_processes:
-        process.ExecuteInitializeSolutionStep()
+gid_output.ExecuteInitializeSolutionStep()
 
-    # Solve step
-    solver.Solve()
+# Solve step
+solver.Solve()
 
-    for process in list_of_processes:
-        process.ExecuteFinalizeSolutionStep()
+gid_output.ExecuteFinalizeSolutionStep()
 
-    for process in list_of_processes:
-        process.ExecuteBeforeOutputStep()
+for process in list_of_processes:
+    process.ExecuteFinalizeSolutionStep()
+
+for process in list_of_processes:
+    process.ExecuteBeforeOutputStep()
     
-    for process in list_of_processes:
-        process.ExecuteAfterOutputStep()
+for process in list_of_processes:
+    process.ExecuteAfterOutputStep()
 
 ## Finalize --------------------------------------------------------------------------------------------------
+
+# Finalizing output files
+eigen_values = [ev for ev in main_model_part.ProcessInfo[KratosStructural.EIGENVALUE_VECTOR]]
+print ("The Eigen Values are:")
+print (eigen_values)
+eigen_utility = KratosStructural.EigenvectorToSolutionStepVariableTransferUtility()
+for step in range(len(eigen_values)):
+    main_model_part.ProcessInfo[KratosMultiphysics.TIME] = float(step+1)
+    eigen_utility.Transfer(main_model_part,step,0)
+    gid_output.PrintOutput()
+
+gid_output.ExecuteFinalize()
 
 for process in list_of_processes:
     process.ExecuteFinalize()
@@ -170,4 +163,3 @@ for process in list_of_processes:
 # Time control
 print("Analysis Completed. Elapsed Time = %.3f" % (timer.perf_counter() - initial_time)," seconds.")
 print(timer.ctime())
-
