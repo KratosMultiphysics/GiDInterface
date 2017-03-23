96b849cd6bcfa44b0e19174d7a4fa7903af604c8
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2017-03-23 16:43:53 +0100

    Inlet function
    Set the correct inlet time and space dependent function.

--- kratos.gid/apps/FSI/examples/MokChannelWithFlexibleWall.tcl
+++ kratos.gid/apps/FSI/examples/MokChannelWithFlexibleWall.tcl
@@ -7,7 +7,7 @@ proc ::FSI::examples::MokChannelFlexibleWall {args} {
 proc FSI::examples::DrawMokChannelFlexibleWallGeometry {args} {
     Kratos::ResetModel
     GiD_Process Mescape 'Layers ChangeName Layer0 Fluid escape
-    
+
     # Geometry creation
     set coordinates [list 0.5 0 0 0 0 0 0 0.5 0 1.75 0.5 0 1.75 0.3 0 1.35 0.3 0 1.3103 0.29881 0 1.2709 0.29453 0 1.232 0.28693 0 1.1937 0.27644 0 1.1565 0.26288 0 0.505 0 0]
     set fluidPoints [list ]
@@ -19,7 +19,7 @@ proc FSI::examples::DrawMokChannelFlexibleWallGeometry {args} {
     foreach {x y z} $coordinates {
         lappend fluidnterfacePoints [GiD_Geometry create point append Fluid $x $y $z]
     }
-    
+
     set fluidLines [list ]
     set initial [lindex $fluidPoints 0]
     foreach point [lrange $fluidPoints 1 end] {
@@ -35,14 +35,14 @@ proc FSI::examples::DrawMokChannelFlexibleWallGeometry {args} {
     #set fluidSurface [GiD_Geometry create surface append plsurface Fluid [llength $fluidLines] {*}$fluidLines]
     set fluidalllines $fluidLines
     lappend fluidalllines {*}$fluidinteractionLines
-    GiD_Process Mescape Geometry Create NurbsSurface {*}$fluidalllines escape escape 
+    GiD_Process Mescape Geometry Create NurbsSurface {*}$fluidalllines escape escape
 
-    
-    GiD_Process 'Layers New Structure escape 
+
+    GiD_Process 'Layers New Structure escape
     GiD_Process 'Layers Off Fluid escape
     GiD_Process 'Layers ToUse Structure escape
-    
-    
+
+
     set coordinates [list 0.505 0 0 0.505 0.25 0 0.5 0.25 0 0.5 0 0 ]
     set strucPoints [list ]
     foreach {x y z} $coordinates {
@@ -55,22 +55,22 @@ proc FSI::examples::DrawMokChannelFlexibleWallGeometry {args} {
         set initial $point
     }
     lappend strucLines [GiD_Geometry create line append stline Structure $initial [lindex $strucPoints 0]]
-    GiD_Process Mescape Geometry Create NurbsSurface {*}$strucLines escape escape 
+    GiD_Process Mescape Geometry Create NurbsSurface {*}$strucLines escape escape
 
-    GiD_Process 'Layers Color Fluid 047186223 Transparent Fluid 255 escape 'Layers Color Structure 187119038 Transparent Structure 255 escape 
+    GiD_Process 'Layers Color Fluid 047186223 Transparent Fluid 255 escape 'Layers Color Structure 187119038 Transparent Structure 255 escape
     GiD_Process 'Layers On Fluid escape
-    
+
     if {$::Model::SpatialDimension eq "3D"} {
         GiD_Process 'Layers Off Structure escape Mescape
         GiD_Process Utilities Copy Surfaces Duplicate DoExtrude Volumes MaintainLayers Translation FNoJoin 0.0,0.0,0.0 FNoJoin 0.0,0.0,0.25 1 escape Mescape
         GiD_Process 'Layers On Structure escape 'Layers Off Fluid escape Mescape
         GiD_Process Utilities Copy Surfaces Duplicate DoExtrude Volumes MaintainLayers Translation FNoJoin 0.0,0.0,0.0 FNoJoin 0.0,0.0,0.25 2 escape Mescape
-        GiD_Process 'Layers On Fluid escape 
-        GiD_Process 'Layers Transparent Fluid 127 escape 
+        GiD_Process 'Layers On Fluid escape
+        GiD_Process 'Layers Transparent Fluid 127 escape
     }
     GiD_Process 'Zoom Frame
-    GiD_Process 'Render Flat escape 
-    
+    GiD_Process 'Render Flat escape
+
     # Group creation
     GiD_Groups create Fluid
     GiD_Groups create Structure
@@ -91,13 +91,13 @@ proc FSI::examples::DrawMokChannelFlexibleWallGeometry {args} {
         GiD_EntitiesGroups assign Inlet surfaces 4
         GiD_EntitiesGroups assign Outlet surfaces 6
         GiD_EntitiesGroups assign NoSlip surfaces {3 7 8 9 10 11 12 13}
-        GiD_EntitiesGroups assign Slip surfaces {1 5 17} 
+        GiD_EntitiesGroups assign Slip surfaces {1 5 17}
         GiD_EntitiesGroups assign FluidFixedDisplacement_full surfaces {3 4 5 6 7 8 9 10 11 12 13}
         GiD_EntitiesGroups assign FluidFixedDisplacement_lat surfaces {1 17}
         GiD_EntitiesGroups assign FluidInterface surfaces {14 15 16}
         GiD_EntitiesGroups assign FixedDisplacement surfaces {21}
         GiD_EntitiesGroups assign StructureInterface surfaces {18 19 20}
-        
+
     } {
         GiD_Groups create FluidALEMeshBC
         GiD_EntitiesGroups assign Fluid surfaces 1
@@ -117,10 +117,10 @@ proc FSI::examples::DrawMokChannelFlexibleWallGeometry {args} {
 
 proc FSI::examples::TreeAssignationMokChannelFlexibleWall {args} {
     set nd $::Model::SpatialDimension
-    
+
     set condtype line
     if {$::Model::SpatialDimension eq "3D"} { set condtype surface }
-    
+
     # Fluid Parts
     set fluidParts {container[@n='FSI']/container[@n='Fluid']/condition[@n='Parts']}
     set fluidNode [spdAux::AddConditionGroupOnXPath $fluidParts Fluid]
@@ -137,11 +137,11 @@ proc FSI::examples::TreeAssignationMokChannelFlexibleWall {args} {
     set fluidConditions {container[@n='FSI']/container[@n='Fluid']/container[@n='BoundaryConditions']}
     # Fluid Interface
     set fluidInlet "$fluidConditions/condition\[@n='AutomaticInlet$nd'\]"
-    
+
     # Fluid Inlet
     set inletNode [spdAux::AddConditionGroupOnXPath $fluidInlet Inlet]
     $inletNode setAttribute ov $condtype
-    set props [list ByFunction No modulus 0.6067 direction automatic_inwards_normal Interval Total]
+    set props [list ByFunction Yes function_modulus {0.1214*(1-cos(0.1*pi*t))*y*(1-y) if t<10 else 0.2428*y*(1-y)} direction automatic_inwards_normal Interval Total]
     foreach {prop val} $props {
          set propnode [$inletNode selectNodes "./value\[@n = '$prop'\]"]
          if {$propnode ne "" } {
@@ -150,7 +150,7 @@ proc FSI::examples::TreeAssignationMokChannelFlexibleWall {args} {
             W "Warning - Couldn't find property Inlet $prop"
         }
     }
-      
+
     # Fluid Outlet
     set fluidOutlet "$fluidConditions/condition\[@n='Outlet$nd'\]"
     set outletNode [spdAux::AddConditionGroupOnXPath $fluidOutlet Outlet]
@@ -164,12 +164,12 @@ proc FSI::examples::TreeAssignationMokChannelFlexibleWall {args} {
             W "Warning - Couldn't find property Outlet $prop"
         }
     }
-    
+
     # Fluid Conditions
     [spdAux::AddConditionGroupOnXPath "$fluidConditions/condition\[@n='NoSlip$nd'\]" NoSlip] setAttribute ov $condtype
     [spdAux::AddConditionGroupOnXPath "$fluidConditions/condition\[@n='Slip$nd'\]" Slip] setAttribute ov $condtype
-    [spdAux::AddConditionGroupOnXPath "$fluidConditions/condition\[@n='FluidNoSlipInterface$nd'\]" FluidInterface] setAttribute ov $condtype  
-    
+    [spdAux::AddConditionGroupOnXPath "$fluidConditions/condition\[@n='FluidNoSlipInterface$nd'\]" FluidInterface] setAttribute ov $condtype
+
     # Displacement 3D
     if {$nd eq "3D"} {
         set fluidDisplacement "$fluidConditions/condition\[@n='ALEMeshDisplacementBC3D'\]"
@@ -212,7 +212,7 @@ proc FSI::examples::TreeAssignationMokChannelFlexibleWall {args} {
 
     # Structural
     gid_groups_conds::setAttributesF {container[@n='FSI']/container[@n='Structural']/container[@n='StageInfo']/value[@n='SolutionType']} {v Dynamic}
-    
+
     # Structural Parts
     set structParts {container[@n='FSI']/container[@n='Structural']/condition[@n='Parts']}
     set structPartsNode [spdAux::AddConditionGroupOnXPath $structParts Structure]
@@ -229,7 +229,7 @@ proc FSI::examples::TreeAssignationMokChannelFlexibleWall {args} {
             W "Warning - Couldn't find property Structure $prop"
          }
     }
-    
+
     # Structural Displacement
     set structDisplacement {container[@n='FSI']/container[@n='Structural']/container[@n='Boundary Conditions']/condition[@n='DISPLACEMENT']}
     set structDisplacementNode [spdAux::AddConditionGroupOnXPath $structDisplacement FixedDisplacement]
@@ -243,7 +243,7 @@ proc FSI::examples::TreeAssignationMokChannelFlexibleWall {args} {
             W "Warning - Couldn't find property Structure $prop"
          }
     }
-    
+
     # Structural Interface
     spdAux::AddConditionGroupOnXPath "container\[@n='FSI'\]/container\[@n='Structural'\]/container\[@n='Loads'\]/condition\[@n='StructureInterface$nd'\]" StructureInterface
 
