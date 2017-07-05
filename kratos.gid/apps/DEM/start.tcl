36cff85c27264957e1d772b4419f171c493dbb8a
Javi Garate <fjgarate@cimne.upc.edu>
2017-07-05 20:42:56 +0200

    #76 some initial files

---
+++ kratos.gid/apps/DEM/start.tcl
@@ -0,0 +1,43 @@
+namespace eval ::DEM {
+    # Variable declaration
+    variable dir
+    variable attributes
+    variable kratos_name
+}
+
+proc ::DEM::Init { } {
+    # Variable initialization
+    variable dir
+    variable attributes
+    variable kratos_name
+    
+    set dir [apps::getMyDir "DEM"]
+    set attributes [dict create]
+    
+    # Allow to open the tree
+    set ::spdAux::TreeVisibility 1
+    
+    # Intervals only in developer mode
+    dict set attributes UseIntervals 1
+    
+    set kratos_name DEMApplication
+    
+    LoadMyFiles
+}
+
+proc ::DEM::LoadMyFiles { } {
+    variable dir
+    
+    uplevel #0 [list source [file join $dir xml GetFromXML.tcl]]
+    uplevel #0 [list source [file join $dir write write.tcl]]
+    uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
+}
+
+proc ::DEM::GetAttribute {name} {
+    variable attributes
+    set value ""
+    if {[dict exists $attributes $name]} {set value [dict get $attributes $name]}
+    return $value
+}
+
+::DEM::Init
---
+++ kratos.gid/apps/DEM/xml/GetFromXML.tcl
@@ -0,0 +1,31 @@
+namespace eval DEM::xml {
+     variable dir
+}
+
+proc DEM::xml::Init { } {
+     variable dir
+     Model::InitVariables dir $DEM::dir
+
+    Model::getSolutionStrategies Strategies.xml
+    Model::getElements Elements.xml
+    Model::getMaterials Materials.xml
+    Model::getNodalConditions NodalConditions.xml
+    Model::getConstitutiveLaws ConstitutiveLaws.xml
+    Model::getProcesses Processes.xml
+    Model::getConditions Conditions.xml
+}
+
+proc DEM::xml::getUniqueName {name} {
+    return DEM$name
+}
+
+proc DEM::xml::MultiAppEvent {args} {
+
+}
+
+proc DEM::xml::CustomTree { args } {
+    
+}
+
+
+DEM::xml::Init
