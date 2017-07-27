41c14385d71467ca6068b6396bd2fa49c2c54f66
Javi Garate <fjgarate@cimne.upc.edu>
2017-07-27 19:36:41 +0200

    #126 Icons for intervals results gravity time parameters and linear solver

--- kratos.gid/apps/Structural/start.tcl
+++ kratos.gid/apps/Structural/start.tcl
@@ -17,8 +17,8 @@ proc ::Structural::Init { } {
     # Allow to open the tree
     set ::spdAux::TreeVisibility 1
     
-    # Intervals only in developer mode
-    dict set attributes UseIntervals 0
+    # Intervals 
+    dict set attributes UseIntervals 1
     if {$::Kratos::kratos_private(DevMode) eq "dev"} {dict set attributes UseIntervals 1}
     
     set kratos_name StructuralMechanicsApplication
