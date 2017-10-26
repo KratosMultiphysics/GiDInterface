2064d7f7fe16171669b9b16020e2fe0230fee22d
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2017-10-26 21:01:17 +0200

    Show up embedded example button

--- kratos.gid/apps/EmbeddedFluid/examples/examples.tcl
+++ kratos.gid/apps/EmbeddedFluid/examples/examples.tcl
@@ -4,10 +4,8 @@ namespace eval EmbeddedFluid::examples {
 
 proc EmbeddedFluid::examples::Init { } {
     uplevel #0 [list source [file join $::EmbeddedFluid::dir examples CylinderInFlow.tcl]]
-    #uplevel #0 [list source [file join $::FSI::dir examples HorizontalFlexibleBar.tcl]]
     GiDMenu::InsertOption "Kratos" [list "---"] 6 PRE "" "" "" replace =
-    GiDMenu::InsertOption "Kratos" [list "Cylinder in air flow" ] 7 PRE [list ::EmbeddedFluid::examples::CylinderInFlow] "" "" replace =
-    #GiDMenu::InsertOption "Kratos" [list "Horizontal flexible bar" ] 8 PRE [list ::FSI::examples::HorizontalFlexibleBar] "" "" replace =
+    GiDMenu::InsertOption "Kratos" [list "Embedded cylinder test" ] 7 PRE [list ::EmbeddedFluid::examples::CylinderInFlow] "" "" replace =
     GiDMenu::UpdateMenus
 }
 
