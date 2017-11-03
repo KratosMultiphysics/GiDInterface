0b0b4c4a42d8978f946b01f43ad15767bc585748
Javi Garate <fjgarate@cimne.upc.edu>
2017-11-03 19:52:28 +0100

    Example geometry, mesh and conditions #161

--- kratos.gid/apps/Structural/examples/TrussCantilever.tcl
+++ kratos.gid/apps/Structural/examples/TrussCantilever.tcl
@@ -58,151 +58,21 @@ proc Structural::examples::AssignTrussCantileverMeshSizes {args} {
 
 
 proc Structural::examples::TreeAssignationTrussCantilever {args} {
-    return ""
     set nd $::Model::SpatialDimension
     set root [customlib::GetBaseRoot]
 
-    set condtype line
-    if {$::Model::SpatialDimension eq "3D"} { set condtype surface }
-
-    # Fluid Parts
-    set fluidParts {container[@n='FSI']/container[@n='Fluid']/condition[@n='Parts']}
-    set fluidNode [spdAux::AddConditionGroupOnXPath $fluidParts Fluid]
-    set props [list Element Monolithic$nd ConstitutiveLaw Newtonian DENSITY 956.0 VISCOSITY 1.51670E-04 YIELD_STRESS 0 POWER_LAW_K 1 POWER_LAW_N 1]
-    foreach {prop val} $props {
-        set propnode [$fluidNode selectNodes "./value\[@n = '$prop'\]"]
-        if {$propnode ne "" } {
-            $propnode setAttribute v $val
-        } else {
-            W "Warning - Couldn't find property Fluid $prop"
-        }
-    }
-
-    set fluidConditions {container[@n='FSI']/container[@n='Fluid']/container[@n='BoundaryConditions']}
-    # Fluid Interface
-    set fluidInlet "$fluidConditions/condition\[@n='AutomaticInlet$nd'\]"
-
-    # Fluid Inlet
-    set inletNode [spdAux::AddConditionGroupOnXPath $fluidInlet Inlet]
-    $inletNode setAttribute ov $condtype
-    set props [list ByFunction Yes function_modulus {0.1214*(1-cos(0.1*pi*t))*y*(1-y) if t<10 else 0.2428*y*(1-y)} direction automatic_inwards_normal Interval Total]
-    foreach {prop val} $props {
-         set propnode [$inletNode selectNodes "./value\[@n = '$prop'\]"]
-         if {$propnode ne "" } {
-              $propnode setAttribute v $val
-         } else {
-            W "Warning - Couldn't find property Inlet $prop"
-        }
-    }
-
-    # Fluid Outlet
-    set fluidOutlet "$fluidConditions/condition\[@n='Outlet$nd'\]"
-    set outletNode [spdAux::AddConditionGroupOnXPath $fluidOutlet Outlet]
-    $outletNode setAttribute ov $condtype
-    set props [list value 0.0]
-    foreach {prop val} $props {
-         set propnode [$outletNode selectNodes "./value\[@n = '$prop'\]"]
-         if {$propnode ne "" } {
-              $propnode setAttribute v $val
-         } else {
-            W "Warning - Couldn't find property Outlet $prop"
-        }
-    }
-
-    # Fluid Conditions
-    [spdAux::AddConditionGroupOnXPath "$fluidConditions/condition\[@n='NoSlip$nd'\]" NoSlip] setAttribute ov $condtype
-    [spdAux::AddConditionGroupOnXPath "$fluidConditions/condition\[@n='Slip$nd'\]" Slip] setAttribute ov $condtype
-    [spdAux::AddConditionGroupOnXPath "$fluidConditions/condition\[@n='FluidNoSlipInterface$nd'\]" FluidInterface] setAttribute ov $condtype
-
-    # Displacement 3D
-    if {$nd eq "3D"} {
-        set fluidDisplacement "$fluidConditions/condition\[@n='ALEMeshDisplacementBC3D'\]"
-        set fluidDisplacementNode [spdAux::AddConditionGroupOnXPath $fluidDisplacement FluidFixedDisplacement_full]
-        $fluidDisplacementNode setAttribute ov surface
-        set props [list constrainedX 1 constrainedY 1 constrainedZ 1 valueX 0.0 valueY 0.0 valueZ 0.0 Interval Total]
-        foreach {prop val} $props {
-             set propnode [$fluidDisplacementNode selectNodes "./value\[@n = '$prop'\]"]
-             if {$propnode ne "" } {
-                  $propnode setAttribute v $val
-             } else {
-                W "Warning - Couldn't find property FluidFixedDisplacement_full $prop"
-             }
-        }
-        set fluidDisplacementNode [spdAux::AddConditionGroupOnXPath $fluidDisplacement FluidFixedDisplacement_lat]
-        $fluidDisplacementNode setAttribute ov surface
-        set props [list constrainedX 0 constrainedY 0 constrainedZ 1 valueX 0.0 valueY 0.0 valueZ 0.0 Interval Total]
-        foreach {prop val} $props {
-             set propnode [$fluidDisplacementNode selectNodes "./value\[@n = '$prop'\]"]
-             if {$propnode ne "" } {
-                  $propnode setAttribute v $val
-             } else {
-                W "Warning - Couldn't find property FluidFixedDisplacement_lat $prop"
-             }
-        }
-    } {
-        set fluidDisplacement "$fluidConditions/condition\[@n='ALEMeshDisplacementBC2D'\]"
-        set fluidDisplacementNode [spdAux::AddConditionGroupOnXPath $fluidDisplacement FluidALEMeshBC]
-        $fluidDisplacementNode setAttribute ov line
-        set props [list constrainedX 1 constrainedY 1 constrainedZ 1 valueX 0.0 valueY 0.0 valueZ 0.0 Interval Total]
-        foreach {prop val} $props {
-             set propnode [$fluidDisplacementNode selectNodes "./value\[@n = '$prop'\]"]
-             if {$propnode ne "" } {
-                  $propnode setAttribute v $val
-             } else {
-                W "Warning - Couldn't find property ALEMeshDisplacementBC2D $prop"
-             }
-        }
-    }
-
-    # Fluid domain time parameters
-    set change_list [list EndTime 25.0 DeltaTime 0.1]
-    set xpath [spdAux::getRoute FLTimeParameters]
-    foreach {name value} $change_list {
-        set node [$root selectNodes "$xpath/value\[@n = '$name'\]"]
-        if {$node ne ""} {
-            $node setAttribute v $value
-        } else {
-            W "Couldn't find $name - Check MOK script"
-        }
-    }
-
-    # Fluid domain output parameters
-    set change_list [list OutputControlType step]
-    set xpath [spdAux::getRoute FLResults]
-    foreach {name value} $change_list {
-        set node [$root selectNodes "$xpath/value\[@n = '$name'\]"]
-        if {$node ne ""} {
-            $node setAttribute v $value
-        } else {
-            W "Couldn't find $name - Check MOK script"
-        }
-    }
-
-    # Fluid monolithic strategy setting
-    spdAux::SetValueOnTreeItem v "Monolithic" FLSolStrat
-
-    # Fluid domain strategy settings
-    set str_change_list [list relative_velocity_tolerance "1e-8" absolute_velocity_tolerance "1e-10" relative_pressure_tolerance "1e-8" absolute_pressure_tolerance "1e-10" maximum_iterations "20"]
-    set xpath [spdAux::getRoute FLStratParams]
-    foreach {name value} $str_change_list {
-        set node [$root selectNodes "$xpath/value\[@n = '$name'\]"]
-        if {$node ne ""} {
-            $node setAttribute v $value
-        } else {
-            W "Couldn't find $name - Check MOK script"
-        }
-    }
+    set condtype point
+    if {$::Model::SpatialDimension eq "3D"} { set condtype line }
 
     # Structural
-    gid_groups_conds::setAttributesF {container[@n='FSI']/container[@n='Structural']/container[@n='StageInfo']/value[@n='SolutionType']} {v Dynamic}
+    # gid_groups_conds::setAttributesF {container[@n='FSI']/container[@n='Structural']/container[@n='StageInfo']/value[@n='SolutionType']} {v Dynamic}
 
     # Structural Parts
-    set structParts {container[@n='FSI']/container[@n='Structural']/condition[@n='Parts']}
+    set structParts {container[@n='Structural']/condition[@n='Parts']}
     set structPartsNode [spdAux::AddConditionGroupOnXPath $structParts Structure]
-    $structPartsNode setAttribute ov [expr {$nd == "3D" ? "volume" : "surface"}]
-    set constLawNameStruc [expr {$nd == "3D" ? "LinearElastic3DLaw" : "LinearElasticPlaneStress2DLaw"}]
-    set props [list Element TotalLagrangianElement$nd ConstitutiveLaw $constLawNameStruc THICKNESS 1.0 DENSITY 1500.0 VISCOSITY 1e-6]
-    lappend props YIELD_STRESS 0 YOUNG_MODULUS 2.3e6 POISSON_RATIO 0.45
+    $structPartsNode setAttribute ov line
+    set constLawNameStruc "TrussConstitutiveLaw"
+    set props [list Element TrussElement$nd ConstitutiveLaw $constLawNameStruc "KratosMultiphysics.StructuralMechanicsApplication.CROSS_AREA" 0.01 DENSITY 1500.0]
     foreach {prop val} $props {
          set propnode [$structPartsNode selectNodes "./value\[@n = '$prop'\]"]
          if {$propnode ne "" } {
@@ -213,9 +83,12 @@ proc Structural::examples::TreeAssignationTrussCantilever {args} {
     }
 
     # Structural Displacement
-    set structDisplacement {container[@n='FSI']/container[@n='Structural']/container[@n='Boundary Conditions']/condition[@n='DISPLACEMENT']}
-    set structDisplacementNode [spdAux::AddConditionGroupOnXPath $structDisplacement FixedDisplacement]
-    $structDisplacementNode setAttribute ov [expr {$nd == "3D" ? "surface" : "line"}]
+    GiD_Groups clone XYZ Total
+    GiD_Groups edit parent Total XYZ
+    spdAux::AddIntervalGroup XYZ "XYZ//Total"
+    set structDisplacement {container[@n='Structural']/container[@n='Boundary Conditions']/condition[@n='DISPLACEMENT']}
+    set structDisplacementNode [spdAux::AddConditionGroupOnXPath $structDisplacement "XYZ//Total"]
+    $structDisplacementNode setAttribute ov point
     set props [list constrainedX Yes ByFunctionX No valueX 0.0 constrainedY Yes ByFunctionY No valueY 0.0 constrainedZ Yes ByFunctionZ No valueZ 0.0]
     foreach {prop val} $props {
          set propnode [$structDisplacementNode selectNodes "./value\[@n = '$prop'\]"]
@@ -226,8 +99,56 @@ proc Structural::examples::TreeAssignationTrussCantilever {args} {
          }
     }
 
-    # Structural Interface
-    spdAux::AddConditionGroupOnXPath "container\[@n='FSI'\]/container\[@n='Structural'\]/container\[@n='Loads'\]/condition\[@n='StructureInterface$nd'\]" StructureInterface
+    # Structural Displacement
+    GiD_Groups clone XZ Total
+    GiD_Groups edit parent Total XZ
+    spdAux::AddIntervalGroup XZ "XZ//Total"
+    set structDisplacement {container[@n='Structural']/container[@n='Boundary Conditions']/condition[@n='DISPLACEMENT']}
+    set structDisplacementNode [spdAux::AddConditionGroupOnXPath $structDisplacement "XZ//Total"]
+    $structDisplacementNode setAttribute ov point
+    set props [list constrainedX Yes ByFunctionX No valueX 0.0 constrainedY No ByFunctionY No valueY 0.0 constrainedZ Yes ByFunctionZ No valueZ 0.0]
+    foreach {prop val} $props {
+         set propnode [$structDisplacementNode selectNodes "./value\[@n = '$prop'\]"]
+         if {$propnode ne "" } {
+              $propnode setAttribute v $val
+         } else {
+            W "Warning - Couldn't find property Structure $prop"
+         }
+    }
+
+    # Structural Displacement
+    GiD_Groups clone Z Total
+    GiD_Groups edit parent Total Z
+    spdAux::AddIntervalGroup Z "Z//Total"
+    set structDisplacement {container[@n='Structural']/container[@n='Boundary Conditions']/condition[@n='DISPLACEMENT']}
+    set structDisplacementNode [spdAux::AddConditionGroupOnXPath $structDisplacement "Z//Total"]
+    $structDisplacementNode setAttribute ov point
+    set props [list constrainedX No ByFunctionX No valueX 0.0 constrainedY No ByFunctionY No valueY 0.0 constrainedZ Yes ByFunctionZ No valueZ 0.0]
+    foreach {prop val} $props {
+         set propnode [$structDisplacementNode selectNodes "./value\[@n = '$prop'\]"]
+         if {$propnode ne "" } {
+              $propnode setAttribute v $val
+         } else {
+            W "Warning - Couldn't find property Structure $prop"
+         }
+    }
+
+    # Point load
+    set structLoad "container\[@n='Structural'\]/container\[@n='Loads'\]/condition\[@n='PointLoad$nd'\]"
+    GiD_Groups clone Load Total
+    GiD_Groups edit parent Total Load
+    spdAux::AddIntervalGroup Load "Load//Total"
+    $structDisplacementNode setAttribute ov point
+    set LoadNode [spdAux::AddConditionGroupOnXPath $structLoad "Load//Total"]
+    set props [list ByFunction No modulus 10000 directionY -1 Interval Total]
+    foreach {prop val} $props {
+         set propnode [$LoadNode selectNodes "./value\[@n = '$prop'\]"]
+         if {$propnode ne "" } {
+              $propnode setAttribute v $val
+         } else {
+            W "Warning - Couldn't find property Structure $prop"
+         }
+    }
 
     # Structure domain time parameters
     set change_list [list EndTime 25.0 DeltaTime 0.1]
@@ -237,63 +158,7 @@ proc Structural::examples::TreeAssignationTrussCantilever {args} {
         if {$node ne ""} {
             $node setAttribute v $value
         } else {
-            W "Couldn't find $name - Check MOK script"
-        }
-    }
-
-    # Structure domain output parameters
-    set change_list [list OutputControlType step]
-    set xpath [spdAux::getRoute STResults]
-    foreach {name value} $change_list {
-        set node [$root selectNodes "$xpath/value\[@n = '$name'\]"]
-        if {$node ne ""} {
-            $node setAttribute v $value
-        } else {
-            W "Couldn't find $name - Check MOK script"
-        }
-    }
-
-    # Structure Bossak scheme setting
-    spdAux::SetValueOnTreeItem v "bossak" STScheme
-
-    # Structure domain strategy settings
-    set str_change_list [list residual_relative_tolerance "1e-8" residual_absolute_tolerance "1e-10" max_iteration "20"]
-    set xpath [spdAux::getRoute STStratParams]
-    foreach {name value} $str_change_list {
-        set node [$root selectNodes "$xpath/value\[@n = '$name'\]"]
-        if {$node ne ""} {
-            $node setAttribute v $value
-        } else {
-            W "Couldn't find $name - Check MOK script"
-        }
-    }
-
-    # Coupling settings
-    set parallelization_parameters [list ParallelSolutionType OpenMP OpenMPNumberOfThreads 4]
-    set parallelization_params_path [spdAux::getRoute "Parallelization"]
-    foreach {n v} $parallelization_parameters {
-        [$root selectNodes "$parallelization_params_path/value\[@n = '$n'\]"] setAttribute v $v
-    }
-
-    set change_list [list nl_tol "1e-8" nl_max_it 25]
-    set xpath [spdAux::getRoute FSIStratParams]
-    foreach {name value} $change_list {
-        set node [$root selectNodes "$xpath/value\[@n = '$name'\]"]
-        if {$node ne ""} {
-            $node setAttribute v $value
-        } else {
-            W "Couldn't find $name - Check MOK script"
-        }
-    }
-
-    set change_list [list Solver MVQN_recursive buffer_size 7]
-    set xpath [spdAux::getRoute FSIDirichletNeumanncoupling_strategy]
-    foreach {name value} $change_list {
-        set node [$root selectNodes "$xpath/value\[@n = '$name'\]"]
-        if {$node ne ""} {
-            $node setAttribute v $value
-        } else {
-            W "Couldn't find $name - Check MOK script"
+            W "Couldn't find $name - Check Truss example script"
         }
     }
 
