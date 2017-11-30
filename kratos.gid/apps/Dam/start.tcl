a1b4c98d40ae211e5f8dcf3f05d517bd880690fd
Lorenzo Gracia <lgracia@cimne.upc.edu>
2017-11-30 17:51:36 +0100

    Computer Aided options

--- kratos.gid/apps/Dam/start.tcl
+++ kratos.gid/apps/Dam/start.tcl
@@ -26,14 +26,34 @@ proc ::Dam::LoadMyFiles { } {
     uplevel #0 [list source [file join $dir xml GetFromXML.tcl]]
     uplevel #0 [list source [file join $dir write write.tcl]]
     uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
-    uplevel #0 [list source [file join $dir examples examples.tcl]]
-    
+    uplevel #0 [list source [file join $dir examples examples.tcl]]   
 }
 
 proc ::Dam::CustomToolbarItems { } {
 
-     Kratos::ToolbarAddItem "Example" "example.png" [list -np- ::Dam::examples::ThermoMechaDam] [= "Example\nThemo-Mechanical Dam"]   
+    Kratos::ToolbarAddItem "Example" "example.png" [list -np- ::Dam::examples::ThermoMechaDam] [= "Example\nThemo-Mechanical Dam"]   
+    if {[ExistsAcombo]} {
+        Kratos::ToolbarAddItem "Parabolic" "example.png" [list -np- ::Dam::LaunchParabolic] [= "Computer Aided\n Parabolic"]
+        Kratos::ToolbarAddItem "Elliptical" "example.png" [list -np- ::Dam::LaunchElliptical] [= "Computer Aided\n Elliptical"]
+    }
+}
+ 
+proc ::Dam::ExistsAcombo { } {
+    set path [GidUtils::GiveProblemTypeFullname ACOMBO]
+    if {[file isdirectory ${path}.gid]} {return 1} 
+    return 0
+}
+ 
+proc ::Dam::LaunchParabolic { } {
+
+    exec GiDx64/13.1.7d/gid -p ACOMBO -t "after 2000 {InputParab}"
+    GiD_Process Mescape Files InsertGeom 
 }
 
+proc ::Dam::LaunchElliptical { } {
+
+    exec GiDx64/13.1.7d/gid -p ACOMBO -t "after 2000 {InputElip}"
+    GiD_Process Mescape Files InsertGeom 
+}
 
 ::Dam::Init
