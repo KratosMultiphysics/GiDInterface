dcfb2d274e8c1e2e714c22a133448f2e9daa8831
Javi Gárate <fjgarate@cimne.upc.edu>
2017-12-28 16:02:52 +0100

    Hotfix

--- kratos.gid/apps/EmbeddedFluid/examples/CylinderInFlow.tcl
+++ kratos.gid/apps/EmbeddedFluid/examples/CylinderInFlow.tcl
@@ -1,6 +1,6 @@
 
 proc ::EmbeddedFluid::examples::CylinderInFlow {args} {
-    if {![IsModelEmpty]} {
+    if {![Kratos::IsModelEmpty]} {
         set txt "We are going to draw the example geometry.\nDo you want to lose your previous work?"
         set retval [tk_messageBox -default ok -icon question -message $txt -type okcancel]
 		if { $retval == "cancel" } { return }
