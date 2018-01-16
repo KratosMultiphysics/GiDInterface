aa91d36efa518a079ec94f827ad7c65ab7c65bbd
Javi Gárate <fjgarate@cimne.upc.edu>
2018-01-16 21:17:16 +0100

    example works #76

--- kratos.gid/apps/DEM/examples/SpheresDrop.tcl
+++ kratos.gid/apps/DEM/examples/SpheresDrop.tcl
@@ -6,8 +6,84 @@ proc ::DEM::examples::SpheresDrop {args} {
 		if { $retval == "cancel" } { return }
     }
 
+    DrawGeometry
+    AssignToTree
+
     GiD_Process 'Redraw
     GidUtils::UpdateWindow GROUPS
     GidUtils::UpdateWindow LAYER
 }
 
+proc ::DEM::examples::DrawGeometry { } {
+    GiD_Process Mescape Geometry Create Object Rectangle -5 -5 0 5 5 0 escape 
+    GiD_Process Mescape Geometry Create Object Rectangle -2 -2 5 2 2 5 escape 
+    GiD_Process Mescape Geometry Create Object Sphere 0 0 2 1 escape 
+
+    GiD_Groups create "Floor"
+    GiD_Groups create "Inlet"
+    GiD_Groups create "Body"
+
+    GiD_EntitiesGroups assign "Floor" surfaces 1
+    GiD_EntitiesGroups assign "Inlet" surfaces 2
+    GiD_EntitiesGroups assign "Body" volumes 1
+}
+
+proc ::DEM::examples::AssignToTree { } {
+    # Parts
+    set DEMParts [spdAux::getRoute "DEMParts"]
+    set DEMPartsNode [spdAux::AddConditionGroupOnXPath $DEMParts Body]
+    set props [list PARTICLE_DENSITY 1.0]
+    foreach {prop val} $props {
+        set propnode [$DEMPartsNode selectNodes "./value\[@n = '$prop'\]"]
+        if {$propnode ne "" } {
+            $propnode setAttribute v $val
+        } else {
+            W "Warning - Couldn't find property Parts $prop"
+        }
+    }
+
+    # DEM FEM Walls
+    set DEMConditions [spdAux::getRoute "DEMConditions"]
+    set walls "$DEMConditions/condition\[@n='DEM-FEM-Wall'\]"
+    set wallsNode [spdAux::AddConditionGroupOnXPath $walls Floor]
+    $wallsNode setAttribute ov surface
+    set props [list PoissonRatio 0.26]
+    foreach {prop val} $props {
+         set propnode [$wallsNode selectNodes "./value\[@n = '$prop'\]"]
+         if {$propnode ne "" } {
+              $propnode setAttribute v $val
+         } else {
+            W "Warning - Couldn't find property Outlet $prop"
+        }
+    }
+
+    # Inlet
+    set DEMInlet "$DEMConditions/condition\[@n='Inlet'\]"
+    set inlets [list Total 2]
+    ErasePreviousIntervals
+    foreach {interval_name modulus} $inlets {
+        GiD_Groups create "Inlet//$interval_name"
+        GiD_Groups edit state "Inlet//$interval_name" hidden
+        spdAux::AddIntervalGroup Inlet "Inlet//$interval_name"
+        set inletNode [spdAux::AddConditionGroupOnXPath $DEMInlet "Inlet//$interval_name"]
+        $inletNode setAttribute ov surface
+        set props [list VELOCITY_MODULUS $modulus Interval $interval_name]
+        foreach {prop val} $props {
+             set propnode [$inletNode selectNodes "./value\[@n = '$prop'\]"]
+             if {$propnode ne "" } {
+                  $propnode setAttribute v $val
+             } else {
+                W "Warning - Couldn't find property Inlet $prop"
+            }
+        }
+    }
+    spdAux::RequestRefresh
+}
+
+proc DEM::examples::ErasePreviousIntervals { } {
+    set root [customlib::GetBaseRoot]
+    set interval_base [spdAux::getRoute "Intervals"]
+    foreach int [$root selectNodes "$interval_base/blockdata\[@n='Interval'\]"] {
+        if {[$int @name] ni [list Initial Total Custom1]} {$int delete}
+    }
+}
