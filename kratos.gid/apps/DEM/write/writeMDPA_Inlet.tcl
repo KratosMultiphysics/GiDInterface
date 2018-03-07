ddca0c6a49a22d423f74c4ae14fe2165466daf0f
Miguel Angel <maceli@cimne.upc.edu>
2018-03-07 15:31:10 +0100

    Bugfix

--- kratos.gid/apps/DEM/write/writeMDPA_Inlet.tcl
+++ kratos.gid/apps/DEM/write/writeMDPA_Inlet.tcl
@@ -54,7 +54,7 @@ proc DEM::write::writeInletMeshes { } {
             lassign [MathUtils::VectorNormalized [list $velocity_X $velocity_Y $velocity_Z]] velocity_X velocity_Y velocity_Z
             lassign [MathUtils::ScalarByVectorProd $velocity [list $velocity_X $velocity_Y $velocity_Z] ] vx vy vz
             write::WriteString "        VELOCITY \[3\] ($vx, $vy, $vz)"
-            write::WriteString "        MAX_RAND_DEVIATION_ANGLE [dict get $inletProperties $groupid DIRECTION_VECTORX]"
+            write::WriteString "        MAX_RAND_DEVIATION_ANGLE [dict get $inletProperties $groupid MAX_RAND_DEVIATION_ANGLE]"
             set type_of_measurement [dict get $inletProperties $groupid FLOW_MEASUREMENT]
             if {$type_of_measurement eq "Kilograms"} {
                 set number_of_particles 200.0
