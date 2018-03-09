74d8c7a9f8f995d14b96e3445b9b8b7c367e8454
Javi Garate <fjgarate@cimne.upc.edu>
2018-03-10 00:31:56 +0100

    Fix Structural example position in menu

--- kratos.gid/apps/Structural/examples/examples.tcl
+++ kratos.gid/apps/Structural/examples/examples.tcl
@@ -4,8 +4,8 @@ namespace eval Structural::examples {
 
 proc Structural::examples::Init { } {
     uplevel #0 [list source [file join $::Structural::dir examples TrussCantilever.tcl]]
-    GiDMenu::InsertOption "Kratos" [list "---"] 6 PRE "" "" "" replace =
-    GiDMenu::InsertOption "Kratos" [list "Truss cantilever" ] 7 PRE [list ::Structural::examples::TrussCantilever] "" "" replace =
+    GiDMenu::InsertOption "Kratos" [list "---"] 8 PRE "" "" "" insertafter =
+    GiDMenu::InsertOption "Kratos" [list "Truss cantilever" ] 8 PRE [list ::Structural::examples::TrussCantilever] "" "" insertafter =
     GiDMenu::UpdateMenus
 }
 
