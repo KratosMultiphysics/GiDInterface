c34c1cd77b3f51637b508861bcdd0413f9e93647
Javi Garate <fjgarate@cimne.upc.edu>
2018-03-10 01:17:22 +0100

    Implementing CustomMenus event for Structural example

--- kratos.gid/apps/Structural/examples/examples.tcl
+++ kratos.gid/apps/Structural/examples/examples.tcl
@@ -4,6 +4,9 @@ namespace eval Structural::examples {
 
 proc Structural::examples::Init { } {
     uplevel #0 [list source [file join $::Structural::dir examples TrussCantilever.tcl]]
+}
+
+proc Structural::examples::UpdateMenus { } {
     GiDMenu::InsertOption "Kratos" [list "---"] 8 PRE "" "" "" insertafter =
     GiDMenu::InsertOption "Kratos" [list "Truss cantilever" ] 8 PRE [list ::Structural::examples::TrussCantilever] "" "" insertafter =
     GiDMenu::UpdateMenus
