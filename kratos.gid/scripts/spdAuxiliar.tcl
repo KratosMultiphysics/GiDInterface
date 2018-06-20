b36a3fc161af6592174dc7771beae0ee64c39234
Javi G <fjgarate@cimne.upc.edu>
2018-06-20 13:10:28 +0200

    Material can be simplified to a combobox #404

--- kratos.gid/scripts/spdAuxiliar.tcl
+++ kratos.gid/scripts/spdAuxiliar.tcl
@@ -1261,17 +1261,6 @@ proc spdAux::getFields {} {
     return $lista
 }
 
-proc spdAux::InsertConstitutiveLawForParameters {input arguments} {
-    return "<value n='ConstitutiveLaw' pn='Constitutive law' v='' actualize_tree='1' values='\[GetConstitutiveLaws\]' dict='\[GetAllConstitutiveLaws\]'  help='Select a constitutive law'>
-        <dependencies node='../value\[@n != ConstitutiveLaw and @n != Element\]' actualize='1'/>
-        </value>
-        <value n='Material' pn='Material' editable='0' help='Choose a material from the database' update_proc='CambioMat' values_tree='\[give_materials_list\]' v='' actualize_tree='1' state='normal'>
-        <edit_command n='Update material data' pn='Update material data' icon='refresh' proc='edit_database_list'/>
-        <dependencies node='../value' actualize='1'/>
-        </value>
-        <dynamicnode command='spdAux::injectPartInputs' args=''/>
-        "
-}
 
 spdAux::Init
 
@@ -1866,6 +1855,35 @@ proc spdAux::PreChargeTree { } {
     }
 }
 
+proc spdAux::InsertMaterialsSimple {domNode args} {
+     return "<value n='Material' pn='Material' editable='0' help='Choose a material from the database' values='\[get_materials_list_simple\]' v='DEM-DefaultMaterial' state='normal' />"
+}
+
+proc spdAux::ProcGet_materials_list_simple {domNode args} {
+    set appid [spdAux::GetAppIdFromNode $domNode]
+    set mats_un [apps::getAppUniqueName $appid Materials]
+    set xp3 [spdAux::getRoute $mats_un]
+    set parentNode [$domNode selectNodes $xp3]
+    set const_law_name [get_domnode_attribute [$domNode selectNodes "../value\[@n = 'ConstitutiveLaw'\]"] v]
+    set filters [list ]
+    if {$const_law_name != ""} {
+        set const_law [Model::getConstitutiveLaw $const_law_name]
+        set filters [$const_law getMaterialFilters]
+    }
+    set materials [Model::GetMaterialsNames $filters]
+
+    set res_raw_list [list ]
+    foreach part [$parentNode selectNodes "./blockdata\[@n = 'material'\]"] {
+        set name [$part @name]
+        if {$name in $materials} {
+            lappend res_raw_list $name
+        }
+    }
+    set v [get_domnode_attribute $domNode v]
+    if {$v ni $res_raw_list} {$domNode setAttribute v $v}
+    return [join $res_raw_list ","]
+}
+
 proc spdAux::ProcGive_materials_list {domNode args} {
     set optional {
         { -has_container container_name "" }
@@ -1885,7 +1903,6 @@ proc spdAux::ProcGive_materials_list {domNode args} {
         set primary_level [lindex $level_names 0]
         set secondary_level [lindex $level_names 1]
         set materials [Model::GetMaterialsNames $filters]
-        WV materials
         if {$secondary_level eq "" && $container_name ne "" && $level == "0"} {
             error [_ "The has_container flag is not available for the database %s (the different types of materials \
                     should be distributed in several containers)" $database_name]     
@@ -1894,22 +1911,24 @@ proc spdAux::ProcGive_materials_list {domNode args} {
         foreach domNode [$parentNode childNodes] {
             set name [$domNode @pn ""]
             if { $name eq "" } { set name [$domNode @name] }
-            if { [$domNode @n] eq "$secondary_level" } {
-                set ret [database_append_list $domNode  $database_name \
-                        [expr {$level+1}] $container_name $icon_name $types_icon_name $filters]
-                if { [llength $ret] } {
-                    lappend l [list $level $name $name $types_icon_name 0]
-                    eval lappend l $ret
-                }
-            } elseif {[$domNode @n] eq "$primary_level"} {
-                set good 1
-                if { $container_name ne "" } {
-                    set xp [format_xpath {container[@n=%s]} $container_name]
-                    if { [$domNode selectNodes $xp] eq "" } { set good 0 }
-                }
-                if { $good } {
-                    
-                    lappend l [list $level $name $name $icon_name 1]
+            if {$name in $materials} {
+                if { [$domNode @n] eq "$secondary_level" } {
+                    set ret [database_append_list $domNode  $database_name \
+                            [expr {$level+1}] $container_name $icon_name $types_icon_name $filters]
+                    if { [llength $ret] } {
+                        lappend l [list $level $name $name $types_icon_name 0]
+                        eval lappend l $ret
+                    }
+                } elseif {[$domNode @n] eq "$primary_level"} {
+                    set good 1
+                    if { $container_name ne "" } {
+                        set xp [format_xpath {container[@n=%s]} $container_name]
+                        if { [$domNode selectNodes $xp] eq "" } { set good 0 }
+                    }
+                    if { $good } {
+                        
+                        lappend l [list $level $name $name $icon_name 1]
+                    }
                 }
             }
         }
@@ -1945,6 +1964,7 @@ proc spdAux::ProcGive_materials_list {domNode args} {
         }
         return [list $primary_level $secondary_level]
     } 
+
     #W $database
     set appid [spdAux::GetAppIdFromNode $domNode]
     set mats_un [apps::getAppUniqueName $appid Materials]
@@ -1963,6 +1983,11 @@ proc spdAux::ProcGive_materials_list {domNode args} {
     }
     
     eval lappend resList [database_append_list $parentNode $database 0 $has_container $icon $types_icon $filters]
+    WV resList
+    set res_raw_list [list ]
+    foreach m $resList {lappend res_raw_list [lindex $m 1]}
+    set v [get_domnode_attribute [$domNode selectNodes "../value\[@n = 'Material'\]"] v]
+    if {$v ni $res_raw_list} {[$domNode selectNodes "../value\[@n = 'Material'\]"] setAttribute v $v}
     return [join $resList ","]
 }
 
