5434d6063844e5b1213e214bc84089fbca787cbd
Javi G <fjgarate@cimne.upc.edu>
2018-06-23 00:30:02 +0200

    Add validations to solid #339 Groups applied to Parts, nodal conditions and conditions must have something

--- kratos.gid/apps/Solid/write/validation.tcl
+++ kratos.gid/apps/Solid/write/validation.tcl
@@ -1,35 +1,120 @@
 
 # return 0 means ok; return [list 1 "Error message to be displayed"]
 proc Solid::write::writeValidateEvent { } {
-    set problem 0
     set problem_message [list ]
     
     # Entities assigned to parts validation
-    set validation [Solid::write::validatePartsMesh]
-    incr problem [lindex $validation 0]
-    lappend problem_message {*}[lindex $validation 1]
+    lappend problem_message {*}[Solid::write::validatePartsMesh]
 
     # Entities assigned to parts validation
-    set validation [Solid::write::validateNodalConditionsMesh]
-    incr problem [lindex $validation 0]
-    lappend problem_message {*}[lindex $validation 1]
+    lappend problem_message {*} [Solid::write::validateNodalConditionsMesh]
 
     # Entities assigned to parts validation
-    set validation [Solid::write::validateLoadsMesh]
-    incr problem [lindex $validation 0]
-    lappend problem_message {*}[lindex $validation 1]
+    lappend problem_message {*} [Solid::write::validateLoadsMesh]
 
-    return [list $problem $problem_message]
+    return [list [llength $problem_message] $problem_message]
 }
 
 proc Solid::write::validatePartsMesh {} {
+    set problem_messages [list ]
     # Get the Parts node
-    set part_un "SLParts"
+    set parts_un "SLParts"
     set root [customlib::GetBaseRoot]
     set xp1 "[spdAux::getRoute $parts_un]/group"
     # Foreach group assigned
     foreach gNode [$root selectNodes $xp1] {
         # Get group name
-        set group_name [$gNode selectNodes "./value\[@n='Material'\]"]
+        set group_name [$gNode @n]
+        # Get the assigned element
+        set element [write::getValueByNode [$gNode selectNodes "./value\[@n = 'Element'\]"]]
+        # Get the element available topologies
+        set topologies [Solid::write::GetTopologies [::Model::getElement $element]]
+        # Validate if the group has any of the valid topologies assigned
+        set has_any [Solid::write::ValidateGroupEmpty $group_name $topologies]
+        if {$has_any == 0} {
+            # Get the topologies to show the message
+            set valid_topologies [list ]
+            foreach topology $topologies {
+                lappend valid_topologies [$topology getGeometry]
+            }
+            # Add the message to the list of problems
+            lappend problem_messages "Parts > group: $group_name must have one of this elements assigned: $valid_topologies. Assign something in geometry and remesh"
+        }
     }
+    return $problem_messages
+}
+proc Solid::write::validateNodalConditionsMesh {} {
+    set problem_messages [list ]
+    # Get the Nodal conditions node
+    set nodal_conditions_un "SLNodalConditions"
+    set root [customlib::GetBaseRoot]
+    set xp1 "[spdAux::getRoute $nodal_conditions_un]/condition/group"
+    # Foreach group assigned
+    foreach gNode [$root selectNodes $xp1] {
+        # Get group name
+        set group_name [$gNode @n]
+        # Get the assigned nodal condition
+        set nodal_condition [[$gNode parent] @n]
+        # Get the nodal condition available topologies
+        set topologies [Solid::write::GetTopologies [Model::getNodalConditionbyId $nodal_condition]]
+        # Validate if the group has any of the valid topologies assigned
+        set has_any [Solid::write::ValidateGroupEmpty $group_name $topologies]
+        if {$has_any == 0} {
+            # Get the topologies to show the message
+            set valid_topologies [list ]
+            foreach topology $topologies {
+                lappend valid_topologies [$topology getGeometry]
+            }
+            # Add the message to the list of problems
+            lappend problem_messages "Conditions > group: $group_name must have one of this elements assigned: $valid_topologies. Assign something in geometry and remesh"
+        }
+    }
+    return $problem_messages
+}
+
+proc Solid::write::validateLoadsMesh {} {
+    set problem_messages [list ]
+    # Get the Conditions node
+    set conditions_un "SLLoads"
+    set root [customlib::GetBaseRoot]
+    set xp1 "[spdAux::getRoute $conditions_un]/condition/group"
+    # Foreach group assigned
+    foreach gNode [$root selectNodes $xp1] {
+        # Get group name
+        set group_name [$gNode @n]
+        # Get the assigned nodal condition
+        set condition [[$gNode parent] @n]
+        # Get the nodal condition available topologies
+        set topologies [Solid::write::GetTopologies [Model::getNodalConditionbyId $condition] ]
+        # Validate if the group has any of the valid topologies assigned
+        set has_any [Solid::write::ValidateGroupEmpty $group_name $topologies]
+        if {$has_any == 0} {
+            # Get the topologies to show the message
+            set valid_topologies [list ]
+            foreach topology $topologies {
+                lappend valid_topologies [$topology getGeometry]
+            }
+            # Add the message to the list of problems
+            lappend problem_messages "Loads > group: $group_name must have one of this elements assigned: $valid_topologies. Assign something in geometry and remesh"
+        }
+    }
+    return $problem_messages
+}
+
+proc Solid::write::GetTopologies { entity } {
+    if {$entity eq ""} {return [list ]}
+    return [$entity getTopologyFeatures]
+}
+proc Solid::write::ValidateGroupEmpty { group_name topologies } {
+    set any 0
+    set isquadratic [write::isquadratic]
+    foreach topology $topologies {
+        set geo [$topology getGeometry]
+        if {[GiD_EntitiesGroups get $group_name elements -count -element_type $geo] > 0} {
+            # TODO: check number of nodes if quadratic
+            set any 1
+            break
+        }
+    }
+    return $any
 }
