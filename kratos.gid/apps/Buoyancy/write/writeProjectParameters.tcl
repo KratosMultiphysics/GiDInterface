750b5549209d2699aef79ad6a8a0ca3cbf41ac90
Javi G <fjgarate@cimne.upc.edu>
2018-08-08 15:31:36 +0200

    Prepare for project parameters

--- kratos.gid/apps/Buoyancy/write/writeProjectParameters.tcl
+++ kratos.gid/apps/Buoyancy/write/writeProjectParameters.tcl
@@ -2,6 +2,26 @@
 proc ::Buoyancy::write::getParametersDict { } {
     set projectParametersDict [dict create]
 
+    # problem data
+    set problem_data_dict [dict create]
+    dict set projectParametersDict problem_data $problem_data_dict
+
+    # output configuration
+    set output_configuration_dict [dict create]
+    dict set projectParametersDict output_configuration $output_configuration_dict
+
+    # restart options
+    set restart_options_dict [dict create]
+    dict set projectParametersDict restart_options $restart_options_dict
+
+    # solver settings
+    set solver_settings_dict [dict create]
+    dict set projectParametersDict solver_settings $solver_settings_dict
+
+    # processes
+    set processes_dict [dict create]
+    dict set projectParametersDict processes $processes_dict
+
     return $projectParametersDict
 }
 
