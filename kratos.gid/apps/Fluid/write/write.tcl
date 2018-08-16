1792c6687b0137c35c5c4e120ac485a23885e67a
Javi G <fjgarate@cimne.upc.edu>
2018-08-16 23:14:05 +0200

    Hotfix fluid memory....

--- kratos.gid/apps/Fluid/write/write.tcl
+++ kratos.gid/apps/Fluid/write/write.tcl
@@ -8,6 +8,7 @@ namespace eval Fluid::write {
 proc Fluid::write::Init { } {
     # Namespace variables inicialization
     variable FluidConditions
+    catch {array unset FluidConditions}
     set FluidConditions(temp) 0
     unset FluidConditions(temp)
 
@@ -117,9 +118,10 @@ proc Fluid::write::Validate {} {
 proc Fluid::write::getLastConditionId { } { 
     variable FluidConditions
     set top 1
-    # Kratos::PrintArray FluidConditions
+     Kratos::PrintArray FluidConditions
     if {[array size FluidConditions]} {
         foreach name [array names FluidConditions] {
+            W "$name $top $FluidConditions($name)"
             set top [expr max($top,$FluidConditions($name))]
         }
     }
