02b6137ed819ae0bcb2b9dbbc729f1cb83c206ba
Javi G <fjgarate@cimne.upc.edu>
2018-09-21 16:19:26 +0200

    Only pfem fluid

--- kratos.gid/apps/DEMPFEM/xml/GetFromXML.tcl
+++ kratos.gid/apps/DEMPFEM/xml/GetFromXML.tcl
@@ -17,43 +17,7 @@ proc DEMPFEM::xml::getUniqueName {name} {
 }
 
 proc DEMPFEM::xml::CustomTree { args } {
-    # spdAux::parseRoutes
-
-    # apps::setActiveAppSoft Fluid
-    # Fluid::xml::CustomTree
-
-    # apps::setActiveAppSoft ConvectionDiffusion
-    # ConvectionDiffusion::xml::CustomTree
-
-    # apps::setActiveAppSoft Buoyancy
-
-    # # Modify the tree: field newValue UniqueName OptionalChild
-    # spdAux::SetValueOnTreeItem v "Monolithic" FLSolStrat
-    # spdAux::SetValueOnTreeItem v "Yes" FLStratParams compute_reactions
-    
-    # # Hide Fluid gravity -> Boussinesq
-    # spdAux::SetValueOnTreeItem state hidden FLGravity
+    spdAux::SetValueOnTreeItem values Fluid PFEMFLUID_DomainType
 }
-
-# Overwriting some procs
-# proc spdAux::injectNodalConditionsOutputs {basenode args} {
-#     set base [$basenode parent]
-#     set args {*}$args
-#     if {$args eq ""} {
-#         set app [apps::getAppById [spdAux::GetAppIdFromNode $base]]
-#         set args [list ImplementedInApplication [join [$app getKratosApplicationName] ","]]
-#     }
-#     return [spdAux::injectNodalConditionsOutputs_do $basenode $args]
-# }
-
-# proc spdAux::injectElementOutputs {basenode args} {
-#     set base [$basenode parent]
-#     set args {*}$args
-#     if {$args eq ""} {
-#         set app [apps::getAppById [spdAux::GetAppIdFromNode $base]]
-#         set args [list ImplementedInApplication [join [$app getKratosApplicationName] ","]]
-#     }
-#     return [spdAux::injectElementOutputs_do $basenode $args]
-# }
 
 DEMPFEM::xml::Init
