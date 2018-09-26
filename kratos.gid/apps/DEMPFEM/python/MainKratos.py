d7a3482a225124bccb491ebea5cc1cdc6aef08ff
Javi G <fjgarate@cimne.upc.edu>
2018-09-26 15:09:53 +0200

    DEMPFEM python

--- kratos.gid/apps/DEMPFEM/python/MainKratos.py
+++ kratos.gid/apps/DEMPFEM/python/MainKratos.py
@@ -1,37 +1,36 @@
+# This script contains an algorithm that models fluid-particle interaction.
+# It combines two parts: a FEM model for the fluid and a DEM model for the particles.
+# It has been conceived by adding the DEM part and the interaction on top of an original fluid-only script (see kratos/applications/FluidDynamicswApplication)
+# Some parts of the original fluid script have been kept practically untouched and are clearly marked.
+# Whenever a minor modification has been made on one of these parts, the corresponding line is indicated with a comment: # MOD.
+
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
-import KratosMultiphysics
-import KratosMultiphysics.FluidDynamicsApplication
-import KratosMultiphysics.ConvectionDiffusionApplication
-
-
-from convection_diffusion_analysis import ConvectionDiffusionAnalysis
-
 import sys
-import time
 
-class ConvectionDiffusionAnalysisWithFlush(ConvectionDiffusionAnalysis):
+# Kratos
+from KratosMultiphysics import *
+from KratosMultiphysics.DEMApplication import *
+from KratosMultiphysics.FluidDynamicsApplication import *
+from KratosMultiphysics.SwimmingDEMApplication import *
+from KratosMultiphysics.DelaunayMeshingApplication import *
+from KratosMultiphysics.SolidMechanicsApplication import *
+from KratosMultiphysics.PfemSolidMechanicsApplication import *
+from KratosMultiphysics.PfemFluidDynamicsApplication import *
+from KratosMultiphysics.ContactMechanicsApplication import *
+from KratosMultiphysics.ExternalSolversApplication import *
 
-    def __init__(self,model,project_parameters,flush_frequency=10.0):
-        super(ConvectionDiffusionAnalysisWithFlush,self).__init__(model,project_parameters)
 
-        self.flush_frequency = flush_frequency
-        self.last_flush = time.time()
+class Solution:
+    def __init__(self, algorithm = None, varying_parameters = Parameters("{}")):
+        self.alg = algorithm
 
-    def FinalizeSolutionStep(self):
-        super(ConvectionDiffusionAnalysisWithFlush,self).FinalizeSolutionStep()
+        if self.alg == None:
+            import swimming_DEM_PFEM_algorithm
+            self.alg = swimming_DEM_PFEM_algorithm.Algorithm(varying_parameters)
 
-        if self.parallel_type == "OpenMP":
-            now = time.time()
-            if now - self.last_flush > self.flush_frequency:
-                sys.stdout.flush()
-                self.last_flush = now
+    def Run(self):
+        return self.alg.Run()
 
-if __name__ == "__main__":
-
-    with open("ProjectParameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
-
-    model = KratosMultiphysics.Model()
-    simulation = ConvectionDiffusionAnalysisWithFlush(model,parameters)
-    simulation.Run()
+if __name__=="__main__":
+    Solution().Run()
