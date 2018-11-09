1060a85812744df74fbdbadb7195aeb9cf36cb7d
Javi Garate <fjgarate@cimne.upc.edu>
2018-11-09 11:39:41 +0100

    Improve CHT tree formation

--- kratos.gid/apps/ConjugateHeatTransfer/xml/GetFromXML.tcl
+++ kratos.gid/apps/ConjugateHeatTransfer/xml/GetFromXML.tcl
@@ -26,49 +26,36 @@ proc ::ConjugateHeatTransfer::xml::MultiAppEvent {args} {
 
 
 proc ConjugateHeatTransfer::xml::CustomTree { args } {
-    return
-    ConjugateHeatTransfer::write::UpdateUniqueNames Fluid
-    apps::setActiveAppSoft Fluid
-    Fluid::xml::CustomTree
+    # Calling to the Buoyancy custom tree event
+    apps::setActiveAppSoft Buoyancy
+    Buoyancy::xml::CustomTree
+    
+    # It seems that we do not need to call the custom tree for the Convection diffusion because
+    # Buoyancy calls it, and it only modifies something in the result section, wich is moved into coupling.
 
-    ConjugateHeatTransfer::write::UpdateUniqueNames Structural
-    apps::setActiveAppSoft Structural
-    Structural::xml::CustomTree
+    # Time intervals only in coupling 
 
-    ConjugateHeatTransfer::write::UpdateUniqueNames ConjugateHeatTransfer
-    apps::setActiveAppSoft ConjugateHeatTransfer
-
-    # Modify the tree: field newValue UniqueName OptionalChild
-    spdAux::SetValueOnTreeItem v "Monolithic" FLSolStrat
-    spdAux::SetValueOnTreeItem v "Yes" FLStratParams compute_reactions
-
-    # Remove Eigen
-    #spdAux::SetValueOnTreeItem values "Static,Quasi-static,Dynamic,formfinding" STSoluType
-
-
-    # Disable MPI parallelism until it is fully tested
-    #spdAux::SetValueOnTreeItem values "OpenMP" ParallelType
 }
 
 # Overwriting some procs
-proc spdAux::injectNodalConditionsOutputs {basenode args} {
-    set base [$basenode parent]
-    set args {*}$args
-    if {$args eq ""} {
-        set app [apps::getAppById [spdAux::GetAppIdFromNode $base]]
-        set args [list ImplementedInApplication [join [$app getKratosApplicationName] ","]]
-    }
-    return [spdAux::injectNodalConditionsOutputs_do $basenode $args]
-}
+# proc spdAux::injectNodalConditionsOutputs {basenode args} {
+#     set base [$basenode parent]
+#     set args {*}$args
+#     if {$args eq ""} {
+#         set app [apps::getAppById [spdAux::GetAppIdFromNode $base]]
+#         set args [list ImplementedInApplication [join [$app getKratosApplicationName] ","]]
+#     }
+#     return [spdAux::injectNodalConditionsOutputs_do $basenode $args]
+# }
 
-proc spdAux::injectElementOutputs {basenode args} {
-    set base [$basenode parent]
-    set args {*}$args
-    if {$args eq ""} {
-        set app [apps::getAppById [spdAux::GetAppIdFromNode $base]]
-        set args [list ImplementedInApplication [join [$app getKratosApplicationName] ","]]
-    }
-    return [spdAux::injectElementOutputs_do $basenode $args]
-}
+# proc spdAux::injectElementOutputs {basenode args} {
+#     set base [$basenode parent]
+#     set args {*}$args
+#     if {$args eq ""} {
+#         set app [apps::getAppById [spdAux::GetAppIdFromNode $base]]
+#         set args [list ImplementedInApplication [join [$app getKratosApplicationName] ","]]
+#     }
+#     return [spdAux::injectElementOutputs_do $basenode $args]
+# }
 
 ConjugateHeatTransfer::xml::Init
