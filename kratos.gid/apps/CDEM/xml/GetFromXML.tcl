2a05c2736433d804181611da2b0dd648f43478be
Unknown <farrufat@cimne.upc.edu>
2018-12-12 16:40:13 +0100

    update

--- kratos.gid/apps/CDEM/xml/GetFromXML.tcl
+++ kratos.gid/apps/CDEM/xml/GetFromXML.tcl
@@ -10,6 +10,7 @@ proc CDEM::xml::Init { } {
 
     Model::ForgetElement SphericPartDEMElement3D
     Model::getElements Elements.xml
+    Model::getProcesses "../../Common/xml/Processes.xml"
 }
 
 proc CDEM::xml::getUniqueName {name} {
@@ -21,12 +22,31 @@ proc CDEM::xml::CustomTree { args } {
     #spdAux::SetValueOnTreeItem values Fluid PFEMFLUID_DomainType
 
     set root [customlib::GetBaseRoot]
-
     set result_node [$root selectNodes "[spdAux::getRoute DEMStratSection]/container\[@n = 'ParallelType'\]"]
 	if { $result_node ne "" } {$result_node delete}
     set result_node [$root selectNodes "[spdAux::getRoute DEMStratSection]/container\[@n = 'DEMGravity'\]"]
 	if { $result_node ne "" } {$result_node delete}
 
+}
+
+proc CDEM::xml::ProcGetElements { domNode args } {
+    set elems [Model::GetElements]
+    set names [list ]
+    set pnames [list ]
+    foreach elem $elems {
+        if {[$elem cumple {*}$args]} {
+            lappend names [$elem getName]
+            lappend pnames [$elem getName]
+            lappend pnames [$elem getPublicName]
+        }
+    }
+    set diction [join $pnames ","]
+    set values [join $names ","]
+    $domNode setAttribute values $values
+    if {[get_domnode_attribute $domNode v] eq ""} {$domNode setAttribute v [lindex $names 0]}
+    if {[get_domnode_attribute $domNode v] ni $names} {$domNode setAttribute v [lindex $names 0]; spdAux::RequestRefresh}
+
+    return $diction
 }
 
 CDEM::xml::Init
