fbf7254a4671ae1245c033f0d01896d81e91dcdd
Javi G <fjgarate@cimne.upc.edu>
2019-03-05 21:38:09 +0100

    lo del esten

--- kratos.gid/apps/Stent/wizard/StentGeometry.tcl
+++ kratos.gid/apps/Stent/wizard/StentGeometry.tcl
@@ -42,8 +42,8 @@ proc Stent::Wizard::DrawGeometry {} {
     set point_distance_row [expr $stent_perimeter/$number_wires]
     set point_distance_column [expr $point_distance_row * tan($degtorad * (90-$angle))]
 
-    set num_rows [expr 1 + ($number_wires *2)]
-    set num_cols [expr 2 + (($stent_length/$point_distance_column) *2)]
+    set num_cols [expr 1 + ($number_wires *2)]
+    set num_rows [expr 2 + (int(double($stent_length)/$point_distance_column) *2)]
 
     array set points_x [FillBidimensionalArray $num_cols $num_rows 0.0]
     array set points_y [FillBidimensionalArray $num_cols $num_rows 0.0]
@@ -82,22 +82,19 @@ proc Stent::Wizard::DrawGeometry {} {
     array set b_points_y [FillBidimensionalArray $num_cols $num_rows 0.0]
     array set b_points_z [FillBidimensionalArray $num_cols $num_rows 0.0]
 
-    set b_points_z(0,0) [expr -1.0 * $wire_diameter]
-
     for {set j 1} {$j < $num_rows} {incr j} {
         if {[expr $j % 2] eq 0} {
             set tj [expr $j-2]
             set b_points_x(0,$j) $b_points_x(0,$tj)
-            set b_points_y(0,$j) [expr $b_points_x(0,$tj) + $point_distance_column]
+            set b_points_y(0,$j) [expr $b_points_y(0,$tj) + $point_distance_column]
             set b_points_z(0,$j) [expr -1.0 * $wire_diameter]
         }
     }
-
     
     for {set j 0} {$j < $num_rows} {incr j} {
-        if {[expr $j % 2] eq 0} {
+        if {[expr $j % 2] == 0 || $j == 0} {
             for {set i 1} {$i < $num_cols} {incr i} {
-                if {[expr $i % 2] eq 0} {
+                if {[expr $i % 2] == 0} {
                     set ti [expr $i-2]
                     set b_points_x($i,$j) [expr $b_points_x($ti,$j) + $point_distance_row]
                     set b_points_y($i,$j) [expr $b_points_y($ti,$j)]
@@ -119,13 +116,12 @@ proc Stent::Wizard::DrawGeometry {} {
 
     set cont1 1
     set c 1
-
     GiD_Geometry -v2 create point $cont1 $layer_name $points_x(0,0) $points_y(0,0) 0.0
     for {set j 0} {$j < $num_rows} {incr j} {
         if {$c eq 1} {set c 0} {set c 1}
 
         for {set i 0} {$i < $num_cols} {incr i} {
-            if {$points_x($i,$j) eq 0 && $points_y($i,$j) eq 0 } {
+            if {$points_x($i,$j) eq 0.0 && $points_y($i,$j) eq 0.0 } {
 
             } else {
                 incr cont1
@@ -141,17 +137,16 @@ proc Stent::Wizard::DrawGeometry {} {
     set cont11 100001
     set g 1
 
-    
-    GiD_Geometry -v2 create point $cont1 $layer_name $b_points_x(0,0) $b_points_y(0,0) 0.0
+    GiD_Geometry -v2 create point $cont11 $layer_name $b_points_x(0,0) $b_points_y(0,0) 0.0
     for {set j 0} {$j < $num_rows} {incr j} {
         if {$g eq 1} {set g 0} {set g 1}
 
         for {set i 0} {$i < $num_cols} {incr i} {
-            if {$b_points_x($i,$j) eq 0 && $b_points_y($i,$j) eq 0 } {
+            if {$b_points_x($i,$j) eq 0.0 && $b_points_y($i,$j) eq 0.0 } {
 
             } else {
                 incr cont11
-                if {$g eq 0 || $j eq $num_rows || $j eq 0} {
+                if {$g eq 1 || $j eq 0 || $j eq $num_rows} {
                     GiD_Geometry -v2 create point $cont11 $layer_name $b_points_x($i,$j) $b_points_y($i,$j) 0.0
                 } else {
                     GiD_Geometry -v2 create point $cont11 $layer_name $b_points_x($i,$j) $b_points_y($i,$j) $b_points_z($i,$j)
@@ -159,9 +154,45 @@ proc Stent::Wizard::DrawGeometry {} {
             }
         }
     }
-
+    
 
     set cont2 0
+    set cont3 [expr $number_wires + 1]
+    set n 0
+    set s 1
+    set d -1
+
+
+    for {set m [expr $number_wires +2]} {$m < [expr $cont1+1]} {incr m} {
+
+        if {$s eq 0 && $cont3 eq $number_wires} {
+            set s 1
+            set cont3 0
+            incr d
+        } elseif {$s eq 1 && $cont3 eq [expr $number_wires + 1]} {                      
+            set s 0
+            set cont3 0
+        }    
+
+        if {$s eq 1} {
+            incr cont3
+            set n [expr int(($m+$d)/($number_wires+1))]
+            if {$m ne [expr ($n*($number_wires +1)+$number_wires -$d)]} {
+                incr cont2
+                set punto1 $m
+                set punto2 [expr $punto1 - $number_wires]
+                GiD_Geometry -v2 create line $cont2 stline $layer_name $punto1 $punto2
+            }
+        } else {
+            incr cont3
+            incr cont2
+            set punto1 $m
+            set punto2 [expr $punto1 - $number_wires]
+            GiD_Geometry -v2 create line $cont2 stline $layer_name $punto1 $punto2
+        }
+    }
+
+    set cont22 100000
     set cont3 [expr $number_wires + 1]
     set n 0
     set s 1
@@ -181,13 +212,7 @@ proc Stent::Wizard::DrawGeometry {} {
 
         if {$s eq 1} {
             incr cont3
-            set aaa [expr $m -100000]
-            set bbb [expr $aaa + $d]
-            set ccc [expr $bbb / ($number_wires+1)]
-            W $ccc
-            W [expr int ($ccc)]
             set n [expr int((($m-100000)+$d)/($number_wires+1))]
-            W $n
             if {[expr $m - 100000] ne [expr ($n*($number_wires +1)-$d)]} {
                 incr cont22
                 set punto1 $m
@@ -201,8 +226,14 @@ proc Stent::Wizard::DrawGeometry {} {
             set punto2 [expr $punto1 - $number_wires - 1]
             GiD_Geometry -v2 create line $cont22 stline $layer_name $punto1 $punto2
         }
-        
+    }
 
+    set cont4 1000000
+    for {set q 2} {$q < $cont1} {incr q} {
+        incr cont4
+        set punto1 $q
+        set punto2 [expr $punto1 +100000]
+        GiD_Geometry -v2 create line $cont4 stline $layer_name $punto1 $punto2
     }
 
     movenodestocylinder
