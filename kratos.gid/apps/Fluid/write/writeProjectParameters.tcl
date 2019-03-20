3d38ae5c91614a183c9197ce9a8948cafce171b2
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2019-03-20 16:42:02 +0100

    Get defaults according to active app id.

--- kratos.gid/apps/Fluid/write/writeProjectParameters.tcl
+++ kratos.gid/apps/Fluid/write/writeProjectParameters.tcl
@@ -3,10 +3,10 @@ proc ::Fluid::write::getParametersDict { } {
     set projectParametersDict [dict create]
 
     # Problem data
-    dict set projectParametersDict problem_data [write::GetDefaultProblemDataDict $Fluid::app_id]
+    dict set projectParametersDict problem_data [write::GetDefaultProblemDataDict [apps::getActiveAppId]]
 
     # output configuration
-    dict set projectParametersDict output_processes [write::GetDefaultOutputProcessDict $Fluid::app_id]
+    dict set projectParametersDict output_processes [write::GetDefaultOutputProcessDict [apps::getActiveAppId]]
 
     # Solver settings
     dict set projectParametersDict solver_settings [Fluid::write::getSolverSettingsDict]
