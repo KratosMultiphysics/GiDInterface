96157b7b299b41b8d0f2c1f6f72db1929cb158ef
Javi G <fjgarate@cimne.upc.edu>
2019-06-04 16:18:44 +0200

    Fix to modern gid

--- kratos.gid/scripts/Menus.tcl
+++ kratos.gid/scripts/Menus.tcl
@@ -131,7 +131,8 @@ proc Kratos::ChangeMenus { } {
     GiDMenu::InsertOption "Kratos" [list "---"] [incr pos] PRE "" "" "" replace =
     GiDMenu::InsertOption "Kratos" [list "Import MDPA"] [incr pos] PRE [list Kratos::ReadPreW] "" "" replace =
     GiDMenu::InsertOption "Kratos" [list "---"] [incr pos] PRE "" "" "" replace =
-    GiDMenu::InsertOption "Kratos" [list "Kratos preferences" ] [incr pos] PRE [list ChangeVariables kratos_preferences] "" "" replace =
+    if {[GidUtils::VersionCmp "14.1.4d"] <0 } { set cmd  [list ChangeVariables kratos_preferences] } {set cmd  [list PreferencesWindow kratos_preferences]}
+    GiDMenu::InsertOption "Kratos" [list "Kratos preferences" ] [incr pos] PRE $cmd "" "" replace =
     GiDMenu::InsertOption "Kratos" [list "About Kratos" ] [incr pos] PREPOST [list Kratos::About] "" "" replace =
     GidChangeDataLabel "Data units" ""
     GidChangeDataLabel "Interval" ""
