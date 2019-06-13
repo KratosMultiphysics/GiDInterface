087ccdbb824dff02bfd69f501a378396db680555
Javi G <fjgarate@cimne.upc.edu>
2019-06-13 20:03:21 +0200

    Init write

--- kratos.gid/apps/Chimera/write/write.tcl
+++ kratos.gid/apps/Chimera/write/write.tcl
@@ -4,35 +4,17 @@ namespace eval Chimera::write {
 
 proc Chimera::write::Init { } {
     # Namespace variables inicialization
-    SetAttribute parts_un FLParts
-    SetAttribute nodal_conditions_un FLNodalConditions
-    SetAttribute conditions_un FLBC
-    SetAttribute materials_un FLMaterials
-    SetAttribute results_un FLResults
-    SetAttribute time_parameters_un FLTimeParameters
-    SetAttribute writeCoordinatesByGroups 0
+    variable writeAttributes
+    set writeAttributes [Fluid::write::GetAttributes]
+
+    SetAttribute writeCoordinatesByGroups 1
     SetAttribute validApps [list "Fluid" "Chimera"]
-    SetAttribute main_script_file "KratosFluid.py"
-    SetAttribute model_part_name "FluidModelPart"
-    SetAttribute materials_file "FluidMaterials.json"
 }
 
 # Events
 proc Chimera::write::writeModelPartEvent { } {
-    # Fluid::write::AddValidApps "Chimera"
-    set err [Fluid::write::Validate]
-    if {$err ne ""} {error $err}
 
-    Fluid::write::InitConditionsMap
-    write::initWriteConfiguration [GetAttributes]
-    write::writeModelPartData
-    Fluid::write::writeProperties
-    write::writeMaterials [GetAttribute validApps]
-    write::writeNodalCoordinatesOnParts
-    write::writeElementConnectivities
-    Fluid::write::writeConditions
-    Fluid::write::writeMeshes
-    Fluid::write::FreeConditionsMap
+    Fluid::write::writeModelPartEvent
 }
 proc Chimera::write::writeCustomFilesEvent { } {
     write::CopyFileIntoModel "python/KratosFluid.py"
