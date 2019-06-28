4d25e7954763c677cc0a62144782bab5572c1bf5
Javi G <fjgarate@cimne.upc.edu>
2019-06-28 16:30:10 +0200

    naming

--- kratos.gid/apps/MPMStructure/write/write.tcl
+++ kratos.gid/apps/MPMStructure/write/write.tcl
@@ -19,13 +19,13 @@ proc MPMStructure::write::writeModelPartEvent { } {
     MPM::write::Init
     MPM::write::SetAttribute writeCoordinatesByGroups 1
     MPM::write::SetAttribute mpm_grid_extra_conditions [list LineMPMInterface2D LineMPMInterface2Da SurfaceMPMInterface3D]
-    MPM::write::RegisterCustomBlockMethod MPMStructure::write::CustomBlock
     write::writeAppMDPA MPM
     set last_mpm_condition [MPM::write::GetLastConditionId]
     
     Structural::write::Init
     Structural::write::SetCoordinatesByGroups 1
     Structural::write::SetAttribute last_condition $last_mpm_condition
+    Structural::write::RegisterCustomBlockMethod MPMStructure::write::CustomBlock
     write::writeAppMDPA Structural
     dict set mdpa_names Structural "${filename}_Structural"
     write::RenameFileInModel "$filename.mdpa" "[dict get $mdpa_names Structural].mdpa"
@@ -39,7 +39,26 @@ proc MPMStructure::write::writeCustomFilesEvent { } {
 }
 
 proc MPMStructure::write::CustomBlock { } {
-    # Time to write the interface properly
+    # Time to write the interface Point condition
+    set root [customlib::GetBaseRoot]
+    if {$::Model::SpatialDimension eq "3D"} {
+        set cnd SurfaceStructureInterface3D
+        set nd 3
+    } else {
+        set cnd LineStructureInterface$Model::SpatialDimension
+        set nd 2
+    }
+    set xp1 "[spdAux::getRoute [Structural::write::GetAttribute conditions_un]]/condition\[@n = '$cnd'\]/group"
+    foreach group [$root selectNodes $xp1] {
+        set groupid [$group @n]
+        # Write the Condition block
+        set intervals [write::writeGroupCondition $groupid PointLoadCondition${nd}D1N 1 [Structural::write::getLastConditionId]]
+        dict set Structural::write::ConditionsDictGroupIterators $groupid $intervals
+        # And the Submodelpart block
+        GiD_Groups clone $groupid ${groupid}_pointelements
+        ::write::writeGroupSubModelPart $cnd ${groupid}_pointelements "Conditions" $intervals
+        GiD_Groups delete ${groupid}_pointelements
+    }
 }
 
 
