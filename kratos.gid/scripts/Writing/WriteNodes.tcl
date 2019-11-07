0d4e299410da739eb474333127326f78c7053f1a
Javi G <fjgarate@cimne.upc.edu>
2019-11-07 11:01:51 +0100

    hotfix if no groups, write no headers

--- kratos.gid/scripts/Writing/WriteNodes.tcl
+++ kratos.gid/scripts/Writing/WriteNodes.tcl
@@ -5,20 +5,23 @@ proc write::writeNodalCoordinatesOnGroups { groups } {
     # Begin Nodes
     # // id          X        Y        Z
     # End Nodes
-    variable formats_dict
-    set id_f [dict get $formats_dict ID]
-    set coord_f [dict get $formats_dict COORDINATE]
-    set formats [dict create]
-    set s [mdpaIndent]
-    WriteString "${s}Begin Nodes"
-    incr ::write::current_mdpa_indent_level
-    foreach group $groups {
-        dict set formats $group "${s}$id_f $coord_f $coord_f $coord_f\n"
+    if {[llength $groups] >0} {
+        variable formats_dict
+        set id_f [dict get $formats_dict ID]
+        set coord_f [dict get $formats_dict COORDINATE]
+        set formats [dict create]
+        set s [mdpaIndent]
+
+        WriteString "${s}Begin Nodes"
+        incr ::write::current_mdpa_indent_level
+        foreach group $groups {
+            dict set formats $group "${s}$id_f $coord_f $coord_f $coord_f\n"
+        }
+        GiD_WriteCalculationFile nodes $formats
+        incr ::write::current_mdpa_indent_level -1
+        WriteString "${s}End Nodes"
+        WriteString "\n"
     }
-    GiD_WriteCalculationFile nodes $formats
-    incr ::write::current_mdpa_indent_level -1
-    WriteString "${s}End Nodes"
-    WriteString "\n"
 }
 
 proc write::writeNodalCoordinatesOnParts { } {
