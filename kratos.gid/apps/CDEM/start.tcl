f1f0ed0e0836ab25a57d0f5c7c6fead5850e66d2
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2019-11-12 18:46:53 +0100

    Added procedure to create and assign bonded domains to all volumes

--- kratos.gid/apps/CDEM/start.tcl
+++ kratos.gid/apps/CDEM/start.tcl
@@ -65,4 +65,23 @@ proc ::CDEM::CustomToolbarItems { } {
     }
 }
 
+proc ::CDEM::CreateAndAssign3DBondedGroups { } {
+
+    # This procedure can be called from the command line of GiD, after loading the CDEM problemtype, just by typing:
+    # -np- ::CDEM::CreateAndAssign3DBondedGroups
+
+    set volume_list [GiD_Geometry -v2 list volume]
+
+    for {set i 0} {$i < [llength $volume_list]} {incr i} {
+
+        set volume_id [lindex $volume_list $i]
+        GiD_Groups create Bonded_domain_groups//SG$volume_id
+        GiD_EntitiesGroups assign Bonded_domain_groups//SG$volume_id volumes $volume_id
+
+        set DEMConditions [spdAux::getRoute "DEMConditions"]
+        set cohesive_cond "$DEMConditions/condition\[@n='DEM-Cohesive'\]"
+        set cohesive_group [customlib::AddConditionGroupOnXPath $cohesive_cond Bonded_domain_groups//SG$volume_id]
+    }
+}
+
 ::CDEM::Init
