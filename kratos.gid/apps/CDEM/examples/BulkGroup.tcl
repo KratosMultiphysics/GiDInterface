401611e12e28678a85b7c43491ff2f1d8b80e591
Javi G <fjgarate@cimne.upc.edu>
2019-11-13 15:24:07 +0100

    Minor details for safety

--- kratos.gid/apps/CDEM/examples/BulkGroup.tcl
+++ kratos.gid/apps/CDEM/examples/BulkGroup.tcl
@@ -18,9 +18,12 @@ proc ::CDEM::examples::BulkGroup {args} {
     GidUtils::UpdateWindow GROUPS
     GidUtils::UpdateWindow LAYER
     GiD_Process 'Zoom Frame
+    
+    spdAux::RequestRefresh
 }
 
 proc ::CDEM::examples::CreateAndAssignBondedGroups { } {
+
     # Prepare the variables
     set entity surface
     set condition_id "DEM-Cohesive2D"
@@ -28,9 +31,14 @@ proc ::CDEM::examples::CreateAndAssignBondedGroups { } {
         set entity volume
         set condition_id "DEM-Cohesive"
     }
+    set group_base Bonded_domain_groups
     # Locate the condition
     set DEMConditions [spdAux::getRoute "DEMConditions"]
     set cohesive_cond "$DEMConditions/condition\[@n='$condition_id'\]"
+
+    # Clear previous assignations
+    if {[GiD_Groups exists $group_base]} {GiD_Groups delete $group_base}
+    gid_groups_conds::delete "$cohesive_cond/group"
 
     # Get the list of entities (surfaces/volumes)
     set entity_list [GiD_Geometry -v2 list $entity]
@@ -40,7 +48,7 @@ proc ::CDEM::examples::CreateAndAssignBondedGroups { } {
         # Get the entity id
         set entity_id [objarray get $entity_list $i] 
         # Create the subgroup
-        set group_id "Bonded_domain_groups//SG$entity_id"
+        set group_id "$group_base//SG$entity_id"
         GiD_Groups create $group_id
         # Assign entity to subgroup
         GiD_EntitiesGroups assign $group_id volumes $entity_id
