3a9143cc056dce2ce6a600a0e142d4e1988c5893
Miguel Angel <maceli@cimne.upc.edu>
2019-12-04 19:09:42 +0100

    Undoing unvoluntary changes

--- kratos.gid/kratos.unix.bat
+++ kratos.gid/kratos.unix.bat
@@ -1,10 +1,10 @@
 #!/bin/bash
 # OutputFile: "$2/$1.info"
 # ErrorFile: "$2/$1.err"
-#delete previous result file 
-rm -f "$2/$1*.post.bin" 
-rm -f "$2/$1*.post.res" 
-rm -f "$2/$1*.post.msh" 
+#delete previous result file
+rm -f "$2/$1*.post.bin"
+rm -f "$2/$1*.post.res"
+rm -f "$2/$1*.post.msh"
 rm -f "$2/$1.info"
 rm -f "$2/$1.err"
 rm -f "$2/$1.flavia.dat"
@@ -20,16 +20,16 @@ fi
 # and maintains OLD_LD_LIBRARY_PATH with previous settings
 # therefore, we use the OLD_LD_LIBRARY_PATH and prepend the path to the kratos libs
 if [ "$OLD_LD_LIBRARY_PATH" != "" ]; then
-    export LD_LIBRARY_PATH="$3/exec/Kratos/bin/Release":"$3/exec/Kratos/bin/Release/libs":$OLD_LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH="$3/exec/Kratos":"$3/exec/Kratos/libs":$OLD_LD_LIBRARY_PATH
 else
     # do not add the ':'
-    export LD_LIBRARY_PATH="$3/exec/Kratos/bin/Release":"$3/exec/Kratos/bin/Release/libs"
+    export LD_LIBRARY_PATH="$3/exec/Kratos":"$3/exec/Kratos/libs"
 fi
 
 # Prevents the PYTHONHOME error from happening and isolate possible python repacks present
 # in the system and interfeering with runkratos
 # export PYTHONHOME="$3/exec/Kratos"
-export PYTHONPATH="$3/exec/Kratos/bin/Releasepython34.zip":"$3/exec/Kratos/bin/Release":$PYTHONPATH
+export PYTHONPATH="$3/exec/Kratos/python34.zip":"$3/exec/Kratos":$PYTHONPATH
 
 
 # if mac
@@ -43,9 +43,6 @@ if [ $KERNEL = "Darwin" ]; then
 else
     KERNEL_NAME="linux"
 fi
-
-echo $LD_LIBRARY_PATH
-echo $PYTHONPATH
 
 # Run Python using the script MainKratos.py
 "$3/exec/Kratos/runkratos" MainKratos.py > "$2/$1.info" 2> "$2/$1.err"
