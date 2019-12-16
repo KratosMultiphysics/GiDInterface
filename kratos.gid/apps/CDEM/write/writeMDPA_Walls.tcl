2e23659e094c3616901e08364f2c21348c9c6e1c
Unknown <farrufat@cimne.upc.edu>
2019-12-16 10:02:11 +0100

    fixed typo, removed unused cdem proc

--- kratos.gid/apps/CDEM/write/writeMDPA_Walls.tcl
+++ kratos.gid/apps/CDEM/write/writeMDPA_Walls.tcl
@@ -47,17 +47,17 @@ proc CDEM::write::WriteWallGraphsFlag { } {
     set xp1 "[spdAux::getRoute [GetAttribute graphs_un]]/group"
     #set xp1 "[spdAux::getRoute [GetAttribute conditions_un]]/condition\[@n = 'DEM-CustomSmp'\]/group"
     foreach group [[customlib::GetBaseRoot] selectNodes $xp1] {
-	set groupid [$group @n]
-	    write::WriteString  "Begin SubModelPart $groupid \/\/ Custom SubModelPart. Group name: $groupid"
-	    write::WriteString  "Begin SubModelPartData // DEM-FEM-Wall. Group name: $groupid"
-	    write::WriteString  "FORCE_INTEGRATION_GROUP 1"
-	    write::WriteString  "End SubModelPartData"
-	    write::WriteString  "Begin SubModelPartNodes"
-	    GiD_WriteCalculationFile nodes -sorted [dict create [write::GetWriteGroupName $groupid] [subst "%10i\n"]]
-	    write::WriteString  "End SubModelPartNodes"
-	    write::WriteString  "End SubModelPart"
-	    write::WriteString  ""
-    }
+        set groupid [$group @n]
+            write::WriteString  "Begin SubModelPart $groupid \/\/ Custom SubModelPart. Group name: $groupid"
+            write::WriteString  "Begin SubModelPartData // DEM-FEM-Wall. Group name: $groupid"
+            write::WriteString  "FORCE_INTEGRATION_GROUP 1"
+            write::WriteString  "End SubModelPartData"
+            write::WriteString  "Begin SubModelPartNodes"
+            GiD_WriteCalculationFile nodes -sorted [dict create [write::GetWriteGroupName $groupid] [subst "%10i\n"]]
+            write::WriteString  "End SubModelPartNodes"
+            write::WriteString  "End SubModelPart"
+            write::WriteString  ""
+        }
 }
 
 proc CDEM::write::GetNodesForGraphs { } {
@@ -66,26 +66,8 @@ proc CDEM::write::GetNodesForGraphs { } {
     #set xp1 "[spdAux::getRoute [GetAttribute graphs_un]]/condition\[@n = 'Graphs'\]/group"
     set xp1 "[spdAux::getRoute [GetAttribute graphs_un]]/group"
     foreach group [[customlib::GetBaseRoot] selectNodes $xp1] {
-	set groupid [$group @n]
-	lappend groups [write::GetWriteGroupName $groupid]
+        set groupid [$group @n]
+        lappend groups [write::GetWriteGroupName $groupid]
     }
     return $groups
-}
-
-# TODO: SHOULD NO LONGER BE REQUIRED SINCE NEW PROC IN DEM::
-proc CDEM::write::writeConditions { wall_properties } {
-    foreach group [DEM::write::GetWallsGroups] {
-        set mid [dict get $wall_properties $group]
-        if {$::Model::SpatialDimension eq "2D"} {
-            set rigid_type "RigidEdge3D2N"
-            set format [write::GetFormatDict $group $mid 2]
-        } else {
-            set rigid_type "RigidFace3D3N"
-            set format [write::GetFormatDict $group $mid 3]
-        }
-        write::WriteString "Begin Conditions $rigid_type // GUI DEM-FEM-Wall group identifier: $group"
-        GiD_WriteCalculationFile connectivities $format
-        write::WriteString "End Conditions"
-        write::WriteString ""
-    }
 }
