6f4e573132be2173e71d6507450a5a159129e7c8
rlrangel <rrangel@cimne.upc.edu>
2020-10-26 18:35:02 +0100

    Writing files of PfemThermic

--- kratos.gid/apps/PfemThermic/write/write.tcl
+++ kratos.gid/apps/PfemThermic/write/write.tcl
@@ -1,50 +1,21 @@
 namespace eval ::PfemThermic::write {
-}
-
-proc ::PfemThermic::write::Init { } {    
     
-    SetAttribute main_script_file "MainKratos.py"
 }
 
-# Events
-proc PfemThermic::write::writeModelPartEvent { } {
+proc ::PfemThermic::write::Init { } {
+    
+}
 
+# MDPA event
+proc PfemThermic::write::writeModelPartEvent { } {
     PfemFluid::write::Init
     PfemFluid::write::writeModelPartEvent
-
-    ConvectionDiffusion::write::Init
-    set ConvectionDiffusion::write::delete_previous_mdpa 0
-    ConvectionDiffusion::write::writeModelPartEvent
 }
 
+# Custom files event
 proc PfemThermic::write::writeCustomFilesEvent { } {
-    SetAttribute main_script_file "MainKratos.py"
-    set orig_name [GetAttribute main_script_file]
-    write::CopyFileIntoModel [file join "python" $orig_name ]
-}
-
-proc PfemThermic::write::WriteMaterialsFile { } {
-    
-}
-
-proc PfemThermic::write::SetAttribute {att val} {
-    ConvectionDiffusion::write::SetAttribute $att $val
-}
-
-proc PfemThermic::write::GetAttribute {att} {
-    return [ConvectionDiffusion::write::GetAttribute $att]
-}
-
-proc PfemThermic::write::GetAttributes {} {
-    return [ConvectionDiffusion::write::GetAttributes]
-}
-
-proc PfemThermic::write::AddAttributes {configuration} {
-    ConvectionDiffusion::write::AddAttributes $configuration
-}
-
-proc PfemThermic::write::AddValidApps {appid} {
-    ConvectionDiffusion::write::AddAttribute validApps $appid
+	PfemFluid::write::writePropertiesJsonFile "PFEMThermicMaterials.json" True [PfemFluid::write::GetAttribute model_part_name]
+    write::CopyFileIntoModel [file join "python" "MainKratos.py"]
 }
 
 PfemThermic::write::Init
