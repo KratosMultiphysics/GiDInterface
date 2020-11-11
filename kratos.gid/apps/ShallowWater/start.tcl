1f6968c4047478e38b7109984242e7a8d22e5da1
Miguel <mmaso@cimne.upc.edu>
2020-11-11 17:10:16 +0100

    initial commit

---
+++ kratos.gid/apps/ShallowWater/start.tcl
@@ -0,0 +1,57 @@
+namespace eval ::ShallowWater {
+    # Variable declaration
+    variable dir
+    variable prefix
+    variable attributes
+    variable app_id
+    variable kratos_name
+}
+
+proc ::ShallowWater::Init { } {
+    # Variable initialization
+    variable dir
+    variable prefix
+    variable attributes
+    variable kratos_name
+    set app_id "ShallowWater"
+    set kratos_name "ShallowWaterApplication"
+
+    apps::LoadAppById "Fluid"
+
+    set dir [apps::getMyDir "ShallowWater"]
+    set attributes [dict create]
+    set prefix SW
+
+    spdAux::SetSpatialDimmension "2D"
+
+    # Allow to open the tree
+    set ::spdAux::TreeVisibility 1
+
+    dict set attributes UseIntervals 0
+
+    LoadMyFiles
+
+    Kratos::AddRestoreVar "::GidPriv(DuplicateEntities)"
+    set ::GidPriv(DuplicateEntities) 1
+}
+
+proc ::ShallowWater::LoadMyFiles { } {
+    variable dir
+
+    uplevel #0 [list source [file join $dir xml XmlController.tcl]]
+#    uplevel #0 [list source [file join $dir write write.tcl]]
+#    uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
+}
+
+proc ::ShallowWater::GetAttribute {name} {
+    variable attributes
+    set value ""
+    catch {set value [dict get $attributes $name]}
+    return $value
+}
+
+proc ::ShallowWater::CustomToolbarItems { } {
+#    Kratos::ToolbarAddItem "Example" "example.png" [list -np- ::ShallowWater::examples::DamBreak] [= "Example\nDamBreak"]
+}
+
+::ShallowWater::Init
---
+++ kratos.gid/apps/ShallowWater/xml/XmlController.tcl
@@ -0,0 +1,38 @@
+namespace eval ShallowWater::xml {
+    # Namespace variables declaration
+    variable dir
+    variable lastImportMeshSize
+    variable export_dir
+}
+
+proc ShallowWater::xml::Init { } {
+    # Namespace variables inicialization
+    variable dir
+    Model::DestroyEverything
+    Model::InitVariables dir $ShallowWater::dir
+
+    Model::getSolutionStrategies Strategies.xml
+    Model::getElements "../../Common/xml/Elements.xml"
+    Model::getConditions "../../Common/xml/Conditions.xml"
+    Model::getProcesses "../../Common/xml/Processes.xml"
+#    Model::getProcesses Processes.xml
+    Model::getSolvers "../../Common/xml/Solvers.xml"
+}
+
+proc ShallowWater::xml::MultiAppEvent {args} {
+   if {$args eq "init"} {
+     spdAux::parseRoutes
+     spdAux::ConvertAllUniqueNames FL ${::ShallowWater::prefix}
+   }
+}
+
+proc ShallowWater::xml::getUniqueName {name} {
+    return ${::ShallowWater::prefix}${name}
+}
+
+proc ShallowWater::xml::CustomTree { args } {
+    spdAux::SetValueOnTreeItem state hidden SWGravity
+    spdAux::SetValueOnTreeItem state hidden SWTimeParameters
+}
+
+ShallowWater::xml::Init
