970580a8fc269c9c0aaa1f2c14b8d46f96347083
Javi Garate <fjgarate@cimne.upc.edu>
2020-11-20 13:42:04 +0100

    missing update

--- kratos.gid/scripts/Controllers/TreeInjections.tcl
+++ kratos.gid/scripts/Controllers/TreeInjections.tcl
@@ -30,6 +30,7 @@ proc spdAux::SetValuesOnBaseNode {base_path prop_value_pairs} {
         set propnode [$base_path selectNodes "./value\[@n = '$prop'\]"]
         if {$propnode ne "" } {
             $propnode setAttribute v $val
+            catch {get_domnode_attribute $propnode dict}
         } else {
             W "Warning - Couldn't find property $prop"
         }
