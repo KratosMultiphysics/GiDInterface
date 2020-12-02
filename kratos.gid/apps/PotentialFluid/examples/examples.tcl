10c181dfd1b32f4a9698dc8e389ee1fd18a3afc9
Javi Garate <fjgarate@cimne.upc.edu>
2020-12-02 18:03:12 +0100

    Remove potential flow

--- kratos.gid/apps/PotentialFluid/examples/examples.tcl
+++
@@ -1,9 +0,0 @@
-namespace eval PotentialFluid::examples {
-
-}
-
-proc PotentialFluid::examples::Init { } {
-    uplevel #0 [list source [file join $::PotentialFluid::dir examples NACA0012.tcl]]
-}
-
-PotentialFluid::examples::Init
