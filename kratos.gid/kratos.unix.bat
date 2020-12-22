bb759689ea1b3c427ad64d2b3a127e25db743861
Javi Garate <fjgarate@cimne.upc.edu>
2020-12-22 12:38:24 +0100

    try

--- kratos.gid/kratos.unix.bat
+++ kratos.gid/kratos.unix.bat
@@ -35,13 +35,17 @@ export PYTHONPATH="$3/exec/Kratos/python34.zip":"$3/exec/Kratos":$PYTHONPATH
 # if mac
 KERNEL=`uname -s`
 if [ $KERNEL = "Darwin" ]; then
-    KERNEL_NAME="macosx"
-    export DYLD_LIBRARY_PATH="$3/exec/Kratos":"$3/exec/Kratos/libs":"$3/exec/Kratos/python-3.kratos.1/lib":$DYLD_LIBRARY_PATH
-    export DYLD_FALLBACK_LIBRARY_PATH="$3/exec/Kratos":"$3/exec/Kratos/libs":$DYLD_FALLBACK_LIBRARY_PATH
-    export PYTHONPATH="$3/exec/Kratos":"$3/exec/Kratos/Lib/lib-dynload/":$PYTHONPATH
-    # export PYTHONHOME="$3/exec/Kratos"
-    # Run Kratos Python using the script MainKratos.py
-    "$3/exec/Kratos/python-3.kratos.1/bin/python3.9" MainKratos.py > "$2/$1.info" 2> "$2/$1.err"
+    if EXIST /usr/local/opt/llvm ; then
+        KERNEL_NAME="macosx"
+        export DYLD_LIBRARY_PATH="$3/exec/Kratos":"$3/exec/Kratos/libs":"$3/exec/Kratos/python-3.kratos.1/lib":$DYLD_LIBRARY_PATH
+        export DYLD_FALLBACK_LIBRARY_PATH="$3/exec/Kratos":"$3/exec/Kratos/libs":$DYLD_FALLBACK_LIBRARY_PATH
+        export PYTHONPATH="$3/exec/Kratos":"$3/exec/Kratos/Lib/lib-dynload/":$PYTHONPATH
+        # export PYTHONHOME="$3/exec/Kratos"
+        # Run Kratos Python using the script MainKratos.py
+        "$3/exec/Kratos/python-3.kratos.1/bin/python3.9" MainKratos.py > "$2/$1.info" 2> "$2/$1.err"
+     else 
+        echo No
+    fi  
 else
     KERNEL_NAME="linux"
     # Run Python using the script MainKratos.py
