177fc87ed6e32a42a27796712ca236e92b888c60
Javi Gárate <fjgarate@cimne.upc.edu>
2020-12-27 13:01:38 +0100

    Add dependencies check

--- kratos.gid/kratos.tcl
+++ kratos.gid/kratos.tcl
@@ -115,6 +115,9 @@ proc Kratos::Event_InitProblemtype { dir } {
     # Start the spd as new project. Mandatory even if we are opening an old case, because this loads the default spd for the future transform
     spdAux::StartAsNewProject
 
+    # Check dependencies
+    Kratos::CheckDependencies
+
     # Open the App selection window. It's delayed to wait if GiD calls the Event_LoadModelSPD (open a case instead of new)
     set activeapp_dom [spdAux::SetActiveAppFromDOM]
     if { $activeapp_dom == "" } {
---
+++ kratos.gid/scripts/Dependencies/macos_brew.sh
@@ -0,0 +1,8 @@
+echo "Installing brew"
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
