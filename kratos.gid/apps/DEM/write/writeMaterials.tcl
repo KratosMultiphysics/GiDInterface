da6d73e9fe321ce9029d2051dd42fc4279421f67
Javi Garate <fjgarate@cimne.upc.edu>
2021-05-04 22:03:46 +0200

    missing todos

--- kratos.gid/apps/DEM/write/writeMaterials.tcl
+++ kratos.gid/apps/DEM/write/writeMaterials.tcl
@@ -3,6 +3,7 @@ proc DEM::write::getDEMMaterialsDict { } {
     # TODO: Add material_ids_list in material_relations
     # TODO: Add SpheresPart RigidFacePart DEMInletPart in material_assignation_table
     # TODO: Remove properties in mdpas ? Check with MA. Probably remove material properties but check the process properties
+    # TODO: Check 2d dem fem wall is written twice, also as phantom
     
     # Loop over parts, inlets and walls to list the materials to print. For each material used print: DENSITY, YOUNG_MODULUS, POISSON_RATIO
     # print COMPUTE_WEAR as false always, too (temporal fix)
