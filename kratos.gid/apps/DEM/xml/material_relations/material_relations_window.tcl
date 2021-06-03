28870b0f056129efdf73cea971ac841ca32de353
Javi Garate <fjgarate@cimne.upc.edu>
2021-06-03 14:29:25 +0200

    table values OK

--- kratos.gid/apps/DEM/xml/material_relations/material_relations_window.tcl
+++ kratos.gid/apps/DEM/xml/material_relations/material_relations_window.tcl
@@ -1,17 +1,7 @@
 
 
 proc DEM::xml::ShowMaterialRelationWindow { } {
-    # window name
-    set w .gid.windowmatrel
     
-    if {[winfo exist $w]} {destroy $w}
-    toplevel $w
-    wm withdraw $w
-    set x [expr [winfo rootx .gid]+[winfo width .gid]/2-[winfo width $w]/2]
-    set y [expr [winfo rooty .gid]+[winfo height .gid]/2-[winfo height $w]/2]
-    wm geom $w +$x+$y
-    wm transient $w .gid    
-    InitWindow $w [_ "Kratos Multiphysics - DEM - Material Relations"] Kratos "" "" 1
 
     set materials [list ]
     foreach mat_node [DEM::write::GetMaterialsNodeList] {
@@ -20,22 +10,51 @@ proc DEM::xml::ShowMaterialRelationWindow { } {
             lappend materials $mat_name
         }
     }
-    W $materials
-    
-    set table $w.tree
-    ttk::treeview $table -columns $materials -displaycolumns $materials
-    foreach header $materials {
-        $table heading $header -text $header -anchor center
-    }
-    pack $table
 
-    set length [llength $materials]
-    for {set i 0} { $i < $length } { incr i } {
-        set row [list]
-        set ref_mat_name [lindex $materials $i]
-        for {set j 0} { $j < $length } { incr j } {
-            lappend row X
+    set relations [dict create]
+
+    foreach relation [DEM::write::GetMaterialRelationsNodeList] {
+        
+        set mat_a [write::getValueByNode [$relation selectNodes "./value\[@n = 'MATERIAL_A'\]"]]
+        set mat_b [write::getValueByNode [$relation selectNodes "./value\[@n = 'MATERIAL_B'\]"]]
+        dict lappend relations $mat_a $mat_b
+        if {$mat_a ne $mat_b} { dict lappend relations $mat_b $mat_a }
+    }
+    
+    if {[llength $materials]>0} {
+        # window name
+        set w .gid.windowmatrel
+        
+        if {[winfo exist $w]} {destroy $w}
+        toplevel $w
+        wm withdraw $w
+        set x [expr [winfo rootx .gid]+[winfo width .gid]/2-[winfo width $w]/2]
+        set y [expr [winfo rooty .gid]+[winfo height .gid]/2-[winfo height $w]/2]
+        wm geom $w +$x+$y
+        wm transient $w .gid    
+        InitWindow $w [_ "Kratos Multiphysics - DEM - Material Relations"] Kratos "" "" 1
+
+    
+        set table $w.tree
+        ttk::treeview $table -columns $materials -displaycolumns $materials
+        foreach header $materials {
+            $table heading $header -text $header -anchor center
         }
-        $table insert "" end -id $ref_mat_name -text $ref_mat_name -values $row
+        pack $table
+
+        set length [llength $materials]
+        for {set i 0} { $i < $length } { incr i } {
+            set row [list]
+            set ref_mat_name [lindex $materials $i]
+            for {set j 0} { $j < $length } { incr j } {
+                set check_mat_name [lindex $materials $j]
+                set exists 0
+                if {[dict exists $relations $ref_mat_name]} {
+                    if {$check_mat_name in [dict get $relations $ref_mat_name]} {set exists 1}
+                }
+                if {$exists} {lappend row V} {lappend row O}
+            }
+            $table insert "" end -id $ref_mat_name -text $ref_mat_name -values $row
+        }
     }
 }
