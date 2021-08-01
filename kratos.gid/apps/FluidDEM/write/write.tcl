67b0aec11c19d89c43a28f7d8eb1d2d8e2a28111
Javi Garate <fjgarate@cimne.upc.edu>
2021-08-01 23:29:30 +0200

    Write permeability as matrix

--- kratos.gid/apps/FluidDEM/write/write.tcl
+++ kratos.gid/apps/FluidDEM/write/write.tcl
@@ -39,7 +39,7 @@ proc FluidDEM::write::writeCustomFilesEvent { } {
 }
 
 proc FluidDEM::write::WriteMaterialsFile { } {
-    Fluid::write::WriteMaterialsFile True
+    FluidDEM::write::writeFluidModifiedMaterials [Fluid::write::GetMaterialsFile True]
     DEM::write::writeMaterialsFile
 }
 
@@ -65,6 +65,23 @@ proc FluidDEM::write::AddValidApps {appid} {
 
 proc Fluid::write::getFluidModelPartFilename { } {
     return [Kratos::GetModelName]Fluid
+}
+
+proc FluidDEM::write::writeFluidModifiedMaterials {fluid_materials_json} {
+    set new_json [dict create]
+    foreach property [dict get $fluid_materials_json properties] {
+        if {[dict exists $property Material Variables PERMEABILITY_11]} {
+            set permeability_1 [list [dict get $property Material Variables PERMEABILITY_11] [dict get $property Material Variables PERMEABILITY_12] [dict get $property Material Variables PERMEABILITY_13]]
+            set permeability_2 [list [dict get $property Material Variables PERMEABILITY_12] [dict get $property Material Variables PERMEABILITY_22] [dict get $property Material Variables PERMEABILITY_23]]
+            set permeability_3 [list [dict get $property Material Variables PERMEABILITY_13] [dict get $property Material Variables PERMEABILITY_23] [dict get $property Material Variables PERMEABILITY_33]]
+            dict unset property Material Variables PERMEABILITY_11; dict unset property Material Variables PERMEABILITY_12; dict unset property Material Variables PERMEABILITY_13
+            dict unset property Material Variables PERMEABILITY_22; dict unset property Material Variables PERMEABILITY_23
+            dict unset property Material Variables PERMEABILITY_33
+            dict set property Material Variables PERMEABILITY [list $permeability_1 $permeability_2 $permeability_3]
+        }
+        dict lappend new_json properties $property
+    }
+    write::writePropertiesJsonFileDone [Fluid::write::GetAttribute materials_file] $new_json
 }
 
 FluidDEM::write::Init
