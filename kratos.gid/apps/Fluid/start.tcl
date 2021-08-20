0f894458c931404715901c13b4a649f835ffbe71
Javi Garate <fjgarate@cimne.upc.edu>
2021-08-20 16:45:03 +0200

    Fluid is the first one usign app.json load

--- kratos.gid/apps/Fluid/start.tcl
+++ kratos.gid/apps/Fluid/start.tcl
@@ -29,18 +29,18 @@ proc ::Fluid::Init { } {
 
     dict set attributes UseIntervals 1
 
-    LoadMyFiles
+    #LoadMyFiles
     #::spdAux::CreateDimensionWindow
 }
 
-proc ::Fluid::LoadMyFiles { } {
-    variable dir
+# proc ::Fluid::LoadMyFiles { } {
+#     variable dir
 
-    uplevel #0 [list source [file join $dir examples examples.tcl]]
-    uplevel #0 [list source [file join $dir xml XmlController.tcl]]
-    uplevel #0 [list source [file join $dir write write.tcl]]
-    uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
-}
+#     uplevel #0 [list source [file join $dir examples examples.tcl]]
+#     uplevel #0 [list source [file join $dir xml XmlController.tcl]]
+#     uplevel #0 [list source [file join $dir write write.tcl]]
+#     uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
+# }
 
 proc ::Fluid::GetAttribute {name} {
     variable attributes
