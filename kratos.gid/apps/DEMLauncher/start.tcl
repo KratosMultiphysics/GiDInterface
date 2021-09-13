ae53912fb4db9081087acb64361f9dde9a44a862
Javi Garate <fjgarate@cimne.upc.edu>
2021-09-13 11:52:30 +0200

    Enables the description

--- kratos.gid/apps/DEMLauncher/start.tcl
+++ kratos.gid/apps/DEMLauncher/start.tcl
@@ -28,7 +28,7 @@ proc ::DemLauncher::AppSelectorWindow { } {
     } {
         [$root selectNodes "value\[@n='nDim'\]"] setAttribute v wait
 
-        set initwind .gid.win_dem_launcher
+        set initwind $::spdAux::application_window_id
         spdAux::DestroyWindows
         spdAux::RegisterWindow $initwind
         toplevel $initwind
@@ -47,21 +47,29 @@ proc ::DemLauncher::AppSelectorWindow { } {
         ttk::frame $w.top
         ttk::label $w.top.title_text -text [_ "Select a DEM application"]
 
-        ttk::frame $w.information  -relief ridge
+        ttk::frame $w.applications  -relief ridge
         set i 0
         foreach app $available_apps {
             set img [::apps::getImgFrom $app]
             set app_publicname [[::apps::getAppById $app] getPublicName]
-            set but [ttk::button $w.information.img$app -image $img -command [list ::DemLauncher::ChangeAppTo $app] ]
-            ttk::label $w.information.text$app -text $app_publicname
-            grid $w.information.img$app -column $i -row 0
-            grid $w.information.text$app -column $i -row 1
+            set but [ttk::button $w.applications.img$app -image $img -command [list ::DemLauncher::ChangeAppTo $app] ]
+            bind $w.applications.img$app <Enter> {::spdAux::PlaceInformationWindowByPath %W applications}
+            ttk::label $w.applications.text$app -text $app_publicname
+            grid $w.applications.img$app -column $i -row 0
+            grid $w.applications.text$app -column $i -row 1
             incr i
         }
         grid $w.top
         grid $w.top.title_text
 
-        grid $w.information
+        grid $w.applications
+
+        # Information panel
+        set spdAux::info_main_window_text ""
+        ttk::labelframe $w.info -text " Information " -relief ridge 
+        ttk::label $w.info.text -textvariable spdAux::info_main_window_text
+        grid $w.info.text
+        grid $w.info -sticky we
     }
 }
 
