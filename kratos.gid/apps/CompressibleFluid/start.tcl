c47abab3f75dbf5b49bc356c81c1e91a7046f47c
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-09-13 16:01:46 +0200

    Removed examples (they were incompressible)

--- kratos.gid/apps/CompressibleFluid/start.tcl
+++ kratos.gid/apps/CompressibleFluid/start.tcl
@@ -36,7 +36,6 @@ proc ::CompressibleFluid::Init { } {
 proc ::CompressibleFluid::LoadMyFiles { } {
     variable dir
 
-    uplevel #0 [list source [file join $dir examples examples.tcl]]
     uplevel #0 [list source [file join $dir xml XmlController.tcl]]
     uplevel #0 [list source [file join $dir write write.tcl]]
     uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
