fa88c630c243efc82309011ec131977378a120b9
Javi Garate <fjgarate@cimne.upc.edu>
2021-09-17 10:45:31 +0200

    base project parameters

--- kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
+++ kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
@@ -1,3 +1,10 @@
 
+proc ::PfemMelting::write::getParametersDict { } {
+    return [::Buoyancy::write::getParametersDict]
+}
 
-
+proc ::PfemMelting::write::writeParametersEvent { } {
+    set projectParametersDict [getParametersDict]
+    write::SetParallelismConfiguration
+    write::WriteJSON $projectParametersDict
+}
