c6d03d00318986297ac5c5d8319b1e443bd89fe9
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-09-17 12:49:56 +0200

    Improved python script

--- kratos.gid/apps/CompressibleFluid/python/MainKratos.py
+++ kratos.gid/apps/CompressibleFluid/python/MainKratos.py
@@ -4,8 +4,8 @@ import time
 import KratosMultiphysics
 from KratosMultiphysics.FluidDynamicsApplication.fluid_dynamics_analysis import FluidDynamicsAnalysis
 
-class FluidDynamicsAnalysisWithFlush(FluidDynamicsAnalysis):
 
+class FluidDynamicsAnalysisWithFlush(FluidDynamicsAnalysis):
     def __init__(self, model, project_parameters, flush_frequency=10.0):
         super().__init__(model, project_parameters)
         self.flush_frequency = flush_frequency
@@ -25,39 +25,55 @@ class FluidDynamicsAnalysisWithFlush(FluidDynamicsAnalysis):
                 sys.stdout.flush()
                 self.last_flush = now
 
-def HasConstrainedArray(params):
+
+def has_constrained_array(params):
     return params.Has("constrained") \
         and params["constrained"].IsArray()
 
-def CleanUpProjectParameters(parameters):
+
+def cleanup_project_parameters(parameters):
     # Unconstraining initial conditions
     initial_processes = parameters["processes"]["initial_conditions_process_list"]
     for process in initial_processes:
         params = process["Parameters"]
-        if HasConstrainedArray(params):
+        if has_constrained_array(params):
             params["value"][2].SetDouble(0.0)
 
             for i in range(3):
                 params["constrained"][i].SetBool(False)
-    
+
     # Unconstraining Z-axis in boundary conditions
     boundary_processes = parameters["processes"]["boundary_conditions_process_list"]
     for process in boundary_processes:
         params = process["Parameters"]
-        if HasConstrainedArray(params):
+        if has_constrained_array(params):
             params["value"][2].SetDouble(0.0)
             params["constrained"][2].SetBool(False)
+
+
+def print_parameters_final(parameters):
+    try:
+        with open("ProjectParameters_as_executed.json", "w+") as f:
+            f.write(parameters.PrettyPrintJsonString())
+    except Exception:
+        print("WARINING: Failed to print ProjectParameters_as_executed.json")
+
 
 if __name__ == "__main__":
 
     with open("ProjectParameters.json", 'r') as parameter_file:
         parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-    CleanUpProjectParameters(parameters)
+    cleanup_project_parameters(parameters)
 
-    global_model = KratosMultiphysics.Model()
-    simulation = FluidDynamicsAnalysisWithFlush(global_model, parameters)
-    simulation.Run()
-
-    with open("ProjectParameters_as_executed.json", "w+") as f:
-        f.write(parameters.PrettyPrintJsonString())
+    try:
+        global_model = KratosMultiphysics.Model()
+        simulation = FluidDynamicsAnalysisWithFlush(global_model, parameters)
+        simulation.Run()
+        # Printing project parameters even if the run failed (to help debug)
+        print_parameters_final(parameters)
+    except Exception as e:
+        print_parameters_final(parameters)
+        raise e
+    finally:
+        print_parameters_final(parameters)
