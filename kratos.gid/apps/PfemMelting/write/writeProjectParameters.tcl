79dfd06c0b75a30843fd449c53a524333fdb7f35
Javi Garate <fjgarate@cimne.upc.edu>
2021-09-20 14:38:36 +0200

    minor

--- kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
+++ kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
@@ -1,10 +1,14 @@
 
 proc ::PfemMelting::write::getParametersDict { } {
-    # Get the ma
+    # Get the project parameters from buoyancy
     set project_parameters_dict [::Buoyancy::write::getParametersDict]
+    
+    # Set laser import settings
     dict set project_parameters_dict problem_data laser_import_settings laser_filename [::write::getValue PFEMMELTING_Laser Parameters_file]
+    # Move fluid material settings
     dict set project_parameters_dict problem_data material_settings [dict get $project_parameters_dict solver_settings fluid_solver_settings material_import_settings]
     dict unset project_parameters_dict solver_settings fluid_solver_settings material_import_settings
+
     return $project_parameters_dict
 }
 
