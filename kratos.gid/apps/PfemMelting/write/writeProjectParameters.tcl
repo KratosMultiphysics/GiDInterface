7ae9f0ee6597a874e91529c6dc440f7dd8880075
Javi Garate <fjgarate@cimne.upc.edu>
2021-09-21 18:54:32 +0200

    write laser not necessary

--- kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
+++ kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
@@ -4,7 +4,7 @@ proc ::PfemMelting::write::getParametersDict { } {
     set project_parameters_dict [::Buoyancy::write::getParametersDict]
     
     # Set laser import settings
-    dict set project_parameters_dict problem_data laser_import_settings laser_filename [::write::getValue PFEMMELTING_Laser Parameters_file]
+    #dict set project_parameters_dict problem_data laser_import_settings laser_filename [::write::getValue PFEMMELTING_Laser Parameters_file]
     # Move fluid material settings
     dict set project_parameters_dict problem_data material_settings [dict get $project_parameters_dict solver_settings fluid_solver_settings material_import_settings]
     dict unset project_parameters_dict solver_settings fluid_solver_settings material_import_settings
