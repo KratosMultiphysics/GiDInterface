bf2d6d79bb308c38a73c96fb34b4698f07bb9721
Javi Garate <fjgarate@cimne.upc.edu>
2021-09-21 18:27:26 +0200

    Add laser path file in example

--- kratos.gid/apps/PfemMelting/examples/cube.tcl
+++ kratos.gid/apps/PfemMelting/examples/cube.tcl
@@ -3,6 +3,7 @@ namespace eval ::PfemMelting::examples::Cube  {
     Kratos::AddNamespace [namespace current]
 
     variable group_body
+    variable group_bottom
 }
 
 proc ::PfemMelting::examples::Cube::Init {args} {
@@ -36,13 +37,16 @@ proc PfemMelting::examples::Cube::DrawGeometry {args} {
 
 # Group assign
 proc PfemMelting::examples::Cube::AssignGroups {args} {
+    # Create the groups
+
     variable group_body
     set group_body body
-    # Create the groups
     GiD_Groups create $group_body
     GiD_Groups edit color $group_body "#26d1a8ff"
     GiD_EntitiesGroups assign $group_body volume 1
 
+    variable group_bottom
+    set group_bottom floor
     GiD_Groups create floor
     GiD_Groups edit color floor "#e0210fff"
     GiD_EntitiesGroups assign floor surface 1
@@ -58,10 +62,22 @@ proc PfemMelting::examples::Cube::AssignGroups {args} {
 }
 
 proc PfemMelting::examples::Cube::TreeAssignation {args} {
-    variable group_body
     # Fluid parts
+    variable group_body
     set fluid_parts_xpath [spdAux::getRoute [::Fluid::GetUniqueName parts]]
     set fluid_node [customlib::AddConditionGroupOnXPath $fluid_parts_xpath $group_body]
     set props [list Element QSVMS3D ConstitutiveLaw Newtonian3DLaw]
     spdAux::SetValuesOnBaseNode $fluid_node $props
+
+    # Laser
+    set laser_filename "LaserSettings_Template.json"
+    set laser_filename_origin [file join $PfemMelting::dir examples $laser_filename]
+    
+    set laser_filename [::FileSelector::_ProcessFile $laser_filename_origin]
+    ::spdAux::SaveModelFile $laser_filename
+    variable group_bottom
+    set laser_xpath [spdAux::getRoute [::ConvectionDiffusion::GetUniqueName conditions]]
+    set laser_node [customlib::AddConditionGroupOnXPath "$laser_xpath/condition\[@n='LaserPath'\]" $group_bottom]
+    set props [list laser_path $laser_filename]
+    spdAux::SetValuesOnBaseNode $laser_node $props
 }
