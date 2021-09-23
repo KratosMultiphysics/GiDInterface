169359e14dd8a0ab585041e6627dd5c9520be2fc
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2021-09-23 11:57:31 +0200

    not running yet

--- kratos.gid/apps/ShallowWater/start.tcl
+++ kratos.gid/apps/ShallowWater/start.tcl
@@ -1,57 +1,22 @@
-namespace eval ::ShallowWater {
+namespace eval ::Shallow {
+    Kratos::AddNamespace [namespace current]
+    
     # Variable declaration
+    variable _app
     variable dir
-    variable prefix
-    variable attributes
-    variable app_id
-    variable kratos_name
+
+    proc GetAttribute {name} {variable _app; return [$_app getProperty $name]}
+    proc GetUniqueName {name} {variable _app; return [$_app getUniqueName $name]}
+    proc GetWriteProperty {name} {variable _app; return [$_app getWriteProperty $name]}
 }
 
-proc ::ShallowWater::Init { } {
+proc ::Shallow::Init {app} {
     # Variable initialization
+    variable _app
     variable dir
-    variable prefix
-    variable attributes
-    variable kratos_name
-    set app_id "ShallowWater"
-    set kratos_name "ShallowWaterApplication"
+    set _app $app
+    set dir [apps::getMyDir "Shallow"]
 
-#    apps::LoadAppById "Fluid"
-
-    set dir [apps::getMyDir "ShallowWater"]
-    set attributes [dict create]
-    set prefix SW
-
-    spdAux::SetSpatialDimmension "2D"
-
-    # Allow to open the tree
-    set ::spdAux::TreeVisibility 1
-
-    dict set attributes UseIntervals 0
-
-    LoadMyFiles
-
-    Kratos::AddRestoreVar "::GidPriv(DuplicateEntities)"
-    set ::GidPriv(DuplicateEntities) 1
+    ::Shallow::xml::Init
+    # ::Shallow::write::Init
 }
-
-proc ::ShallowWater::LoadMyFiles { } {
-    variable dir
-
-    uplevel #0 [list source [file join $dir xml XmlController.tcl]]
-#    uplevel #0 [list source [file join $dir write write.tcl]]
-#    uplevel #0 [list source [file join $dir write writeProjectParameters.tcl]]
-}
-
-proc ::ShallowWater::GetAttribute {name} {
-    variable attributes
-    set value ""
-    catch {set value [dict get $attributes $name]}
-    return $value
-}
-
-proc ::ShallowWater::CustomToolbarItems { } {
-#    Kratos::ToolbarAddItem "Example" "example.png" [list -np- ::ShallowWater::examples::DamBreak] [= "Example\nDamBreak"]
-}
-
-::ShallowWater::Init
--- kratos.gid/apps/ShallowWater/xml/XmlController.tcl
+++ kratos.gid/apps/ShallowWater/xml/XmlController.tcl
@@ -1,17 +1,11 @@
-namespace eval ShallowWater::xml {
-    # Namespace variables declaration
-    variable dir
-    variable lastImportMeshSize
-    variable export_dir
+namespace eval ::Shallow::xml {
+    namespace path ::Shallow
+    Kratos::AddNamespace [namespace current]
 }
 
-proc ShallowWater::xml::Init { } {
-    # Namespace variables inicialization
-    variable dir
-    Model::DestroyEverything
-    Model::InitVariables dir $ShallowWater::dir
+proc ::Shallow::xml::Init {} {
+    Model::InitVariables dir $::Shallow::dir
 
-    Model::getSolutionStrategies Strategies.xml
     Model::getElements "../../Common/xml/Elements.xml"
     Model::getConditions Conditions.xml
     Model::getNodalConditions NodalConditions.xml
@@ -20,20 +14,11 @@ proc ShallowWater::xml::Init { } {
     Model::getSolvers "../../Common/xml/Solvers.xml"
 }
 
-# proc ShallowWater::xml::MultiAppEvent {args} {
-#    if {$args eq "init"} {
-#      spdAux::parseRoutes
-#      spdAux::ConvertAllUniqueNames FL ${::ShallowWater::prefix}
-#    }
-# }
-
-proc ShallowWater::xml::getUniqueName {name} {
-    return ${::ShallowWater::prefix}${name}
+proc ::Shallow::xml::getUniqueName {name} {
+    return [::Shallow::GetAttribute prefix]${name}
 }
 
-# proc ShallowWater::xml::CustomTree { args } {
+# proc ::Shallow::xml::CustomTree {args} {
     # spdAux::SetValueOnTreeItem state normal FLGravity
     # spdAux::SetValueOnTreeItem state normal FLTimeParameters
 # }
-
-ShallowWater::xml::Init
