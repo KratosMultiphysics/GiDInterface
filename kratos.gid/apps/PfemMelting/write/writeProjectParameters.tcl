54cde1debb2217690ed815b25176ddf68ade3520
Javi Garate <fjgarate@cimne.upc.edu>
2021-09-24 14:34:38 +0200

    reform_dofs_at_each_step and variables

--- kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
+++ kratos.gid/apps/PfemMelting/write/writeProjectParameters.tcl
@@ -4,12 +4,18 @@ proc ::PfemMelting::write::getParametersDict { } {
     set project_parameters_dict [::Buoyancy::write::getParametersDict]
     
     # Set laser import settings
-    #dict set project_parameters_dict problem_data laser_import_settings laser_filename [::write::getValue PFEMMELTING_Laser Parameters_file]
+    # dict set project_parameters_dict problem_data laser_import_settings laser_filename [::write::getValue PFEMMELTING_Laser Parameters_file]
     # Move fluid material settings
     dict set project_parameters_dict problem_data material_settings [dict get $project_parameters_dict solver_settings fluid_solver_settings material_import_settings]
     dict unset project_parameters_dict solver_settings fluid_solver_settings material_import_settings
     # Add InitialTemperature process using Parts submodelpart
     # Copy Ambient temperature to the processes
+
+    # Set reform_dofs_at_each_step
+    dict set project_parameters_dict solver_settings thermal_solver_settings reform_dofs_at_each_step false
+    dict set project_parameters_dict solver_settings fluid_solver_settings reform_dofs_at_each_step false
+    dict set project_parameters_dict solver_settings fluid_solver_settings alpha 0.0
+    dict set project_parameters_dict solver_settings fluid_solver_settings move_mesh_strategy 2
 
     return $project_parameters_dict
 }
