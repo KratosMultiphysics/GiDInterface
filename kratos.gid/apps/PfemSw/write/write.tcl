a8fc6002bcf5a087bbacb4f7b6e826142110f751
migelmaso <mmaso@cimne.upc.edu>
2022-09-08 12:52:58 +0200

    definition of coupling processes

--- kratos.gid/apps/PfemSw/write/write.tcl
+++ kratos.gid/apps/PfemSw/write/write.tcl
@@ -1,7 +1,7 @@
 namespace eval ::PfemSw::write {
     namespace path ::PfemSw
     Kratos::AddNamespace [namespace current]
-    
+
     variable pfem_project_parameters
     variable sw_project_parameters
     variable mdpa_names
@@ -12,7 +12,7 @@ proc ::PfemSw::write::Init { } {
     variable sw_project_parameters
     set pfem_project_parameters [dict create ]
     set sw_project_parameters [dict create ]
-    
+
     variable mdpa_names
     set mdpa_names [dict create ]
 }
@@ -21,17 +21,19 @@ proc ::PfemSw::write::Init { } {
 proc ::PfemSw::write::writeModelPartEvent { } {
     variable mdpa_names
     set filename [Kratos::GetModelName]
-    
+
+    # check folder exists
+
     PfemFluid::write::Init
     PfemFluid::write::InitConditionsMap
     PfemFluid::write::SetCoordinatesByGroups 1
     write::writeAppMDPA PfemFluid
     dict set mdpa_names PfemFluid "${filename}_PFEM"
-    write::RenameFileInModel "$filename.mdpa" "[dict get $mdpa_names PfemFluid].mdpa"
-    
+    write::RenameFileInModel "$filename.mdpa" "pfem/[dict get $mdpa_names PfemFluid].mdpa"
+
     ShallowWater::write::Init
     ShallowWater::write::SetCoordinatesByGroups 1
     write::writeAppMDPA ShallowWater
     dict set mdpa_names ShallowWater "${filename}_SW"
-    write::RenameFileInModel "$filename.mdpa" "[dict get $mdpa_names ShallowWater].mdpa"
+    write::RenameFileInModel "$filename.mdpa" "sw/[dict get $mdpa_names ShallowWater].mdpa"
 }
