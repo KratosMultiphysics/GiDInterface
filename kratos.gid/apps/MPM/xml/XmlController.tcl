3bd51487ff9e678df4621927cbc1740fb29c3328
Franci <falessandro@cimne.upc.edu>
2022-11-28 10:40:11 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 3d88ec97ba445523754f3ed9cf7fe929b865ed0d.

--- kratos.gid/apps/MPM/xml/XmlController.tcl
+++ kratos.gid/apps/MPM/xml/XmlController.tcl
@@ -12,9 +12,6 @@ proc MPM::xml::Init { } {
     Model::ForgetElements
     Model::getElements Elements.xml
 
-    Model::ForgetSolutionStrategies
-    Model::getSolutionStrategies Strategies.xml
-
     # Modify the schemes so more elements are filtered
     foreach strategy $::Model::SolutionStrategies {
         $strategy setAttribute NeedElements false
@@ -60,8 +57,8 @@ proc MPM::xml::getUniqueName {name} {
 
 proc MPM::xml::CustomTree { args } {
 
-#     spdAux::SetValueOnTreeItem v "time" Results OutputControlType
-#     spdAux::SetValueOnTreeItem values "time" Results OutputControlType
+    spdAux::SetValueOnTreeItem v "time" Results OutputControlType
+    spdAux::SetValueOnTreeItem values "time" Results OutputControlType
     spdAux::SetValueOnTreeItem v No NodalResults PARTITION_INDEX
     spdAux::SetValueOnTreeItem v "LinearSolversApplication.sparse_lu" MPMimplicitlinear_solver_settings Solver
 }
