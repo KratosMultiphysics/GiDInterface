4303caf943eeeb24091e32595e9bd49af6643df8
Javi Garate <fjgarate@cimne.upc.edu>
2022-12-14 20:33:25 +0100

    z not shown in 2D

--- kratos.gid/apps/CompressibleFluid/xml/XmlController.tcl
+++ kratos.gid/apps/CompressibleFluid/xml/XmlController.tcl
@@ -6,10 +6,10 @@ namespace eval CompressibleFluid::xml {
 proc ::CompressibleFluid::xml::Init { } {
     # Namespace variables inicialization
     Model::InitVariables dir $::CompressibleFluid::dir
-    
+
     set FluidXML  "$::Fluid::dir/xml"
     set CommonXML "../../Common/xml"
-    
+
     Model::getSolutionStrategies Strategies.xml
     Model::getElements           Elements.xml
     Model::getMaterials          Materials.xml
@@ -17,10 +17,10 @@ proc ::CompressibleFluid::xml::Init { } {
     Model::ForgetNodalConditions
     Model::getNodalConditions    "$FluidXML/NodalConditions.xml"
     Model::getNodalConditions    NodalConditions.xml
-    
+
     Model::ForgetConstitutiveLaws
     Model::getConstitutiveLaws   ConstitutiveLaws.xml
-    
+
     Model::getProcesses          "$CommonXML/Processes.xml"
     Model::getProcesses          "$FluidXML/Processes.xml"
 
@@ -46,13 +46,25 @@ proc ::CompressibleFluid::xml::CustomTree { args } {
     if {[$root selectNodes "$xpath/condition\[@n='Drag'\]"] eq ""} {
         gid_groups_conds::addF $xpath include [list n Drag active 1 path {apps/Fluid/xml/Drag.spd}]
     }
-    
+
     customlib::ProcessIncludes $::Kratos::kratos_private(Path)
     spdAux::parseRoutes
 
     # Nodal reactions in output settings
     if {[$root selectNodes "$xpath/container\[@n='OnNodes'\]"] ne ""} {
         gid_groups_conds::addF "$xpath/container\[@n='OnNodes'\]" value [list n REACTION pn "Reaction" v No values "Yes,No"]
+    }
+
+    set nDim $::Model::SpatialDimension
+    if {$nDim ne "3D"} {
+        set xpath "[spdAux::getRoute CFBC]/condition\[@n='MomentumConstraints2D'\]"
+        if {[$root selectNodes "$xpath"] ne ""} {
+            [$momentum_node selectNodes "./value\[@n = 'selector_component_Z'\]"] setAttribute v "Not"
+        }
+        set xpath "[spdAux::getRoute FLNodalConditions]/condition\[@n='MOMENTUM'\]"
+        if {[$root selectNodes "$xpath"] ne ""} {
+            [$momentum_node selectNodes "./value\[@n = 'selector_component_Z'\]"] setAttribute v "Not"
+        }
     }
 }
 
