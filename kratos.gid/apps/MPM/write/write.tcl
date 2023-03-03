df7ed45898644f77324864ae209d3fd6f9f6a458
Javi Gárate <fjgarate@cimne.upc.edu>
2023-03-03 17:51:50 +0100

    hide and show if element

--- kratos.gid/apps/MPM/write/write.tcl
+++ kratos.gid/apps/MPM/write/write.tcl
@@ -90,10 +90,9 @@ proc MPM::write::GetPartsGroups { part_type {what "name"} } {
     return $body_groups
 }
 
-
 proc ::MPM::write::GetUsedElements { {get "Objects"} } {
     set lista [list ]
-    foreach gNode [write::getPartsGroupsId node] {
+    foreach gNode [MPM::write::GetPartsGroups Body node] {
         set elem_name [write::getValueByNode [$gNode selectNodes ".//value\[@n='Element']"] ]
         set e [Model::getElement $elem_name]
         if {$get eq "Name"} { set e [$e getName] }
@@ -101,7 +100,6 @@ proc ::MPM::write::GetUsedElements { {get "Objects"} } {
     }
     return $lista
 }
-
 
 proc MPM::write::writeBodyNodalCoordinates { } {
     write::writeNodalCoordinatesOnGroups [MPM::write::GetPartsGroups Body]
