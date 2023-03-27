6f9959dac12f2b22b5f41f5cbba7833594935891
Javi Gárate <fjgarate@cimne.upc.edu>
2023-03-27 11:46:49 +0200

    comment

--- kratos.gid/apps/GeoMechanics/controllers/Toolbar.tcl
+++ kratos.gid/apps/GeoMechanics/controllers/Toolbar.tcl
@@ -3,7 +3,8 @@ namespace eval ::GeoMechanics::toolbar {
     Kratos::AddNamespace [namespace current]
 }
 
+# This application has it's own toolbar, so common toolbar is disabled and here we can find the custom toolbar implementation
 proc ::GeoMechanics::toolbar::Init { } {
-    W "toolbar"
+    
 
 }
