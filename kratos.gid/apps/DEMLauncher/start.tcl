125b76a2ccc0dc289db0a90d068af37ab9701ba1
Javi Garate <fjgarate@cimne.upc.edu>
2023-04-13 12:39:00 +0200

    remove window management

--- kratos.gid/apps/DEMLauncher/start.tcl
+++ kratos.gid/apps/DEMLauncher/start.tcl
@@ -28,16 +28,7 @@ proc ::DemLauncher::AppSelectorWindow { } {
         set initwind $::spdAux::application_window_id
         spdAux::DestroyWindows
         spdAux::RegisterWindow $initwind
-        toplevel $initwind
-        wm withdraw $initwind
-
         set w $initwind
-
-        set x [expr [winfo rootx .gid]+[winfo width .gid]/2-[winfo width $w]/2]
-        set y [expr [winfo rooty .gid]+[winfo height .gid]/2-[winfo height $w]/2]
-
-        wm geom $initwind +$x+$y
-        wm transient $initwind .gid
 
         InitWindow $w [_ "DEM applications"] Kratos "" "" 1
         set initwind $w
