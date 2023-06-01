4f5785d36262938e439edd468cbf707e620083a9
Javi Gárate <fjgarate@cimne.upc.edu>
2023-06-01 17:35:19 +0200

    prepare for proj params

--- kratos.gid/apps/GeoMechanics/write/writeProjectParameters.tcl
+++ kratos.gid/apps/GeoMechanics/write/writeProjectParameters.tcl
@@ -1,18 +1,20 @@
-# Project Parameters
-
-proc ::GeoMechanics::write::writeParametersEvent { } {
-    write::WriteJSON [getParametersDict]
-
-}
 
 # Project Parameters
-proc ::GeoMechanics::write::getParametersDict { } {
+proc ::GeoMechanics::write::getParametersDict { stage } {
     # Get the base dictionary for the project parameters
     set project_parameters_dict [dict create]
+
+    
 
     return $project_parameters_dict
 }
 
 proc ::GeoMechanics::write::writeParametersEvent { } {
-    write::WriteJSON [::GeoMechanics::write::getParametersDict]
+    
+    set stages [::GeoMechanics::xml::GetStages]
+    foreach stage $stages {
+        write::CloseFile
+        write::OpenFile "ProjectParameters[$stage @name].json"
+        write::WriteJSON [::GeoMechanics::write::getParametersDict $stage]
+    }
 }
