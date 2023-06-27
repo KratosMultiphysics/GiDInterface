e2e607f5b66c0f9ea70da87afbb74b3b58e351c8
Javi Gárate <fjgarate@cimne.upc.edu>
2023-06-27 17:25:24 +0200

    Add pyhton button

--- kratos.gid/apps/GeoMechanics/controllers/Python.tcl
+++ kratos.gid/apps/GeoMechanics/controllers/Python.tcl
@@ -1,3 +1,6 @@
 proc ::GeoMechanics::PythonButton { } {
-    W "proc called"
+    set param1 "example parameter"
+    GiD_Python_Source [file join $::GeoMechanics::dir controllers geomechanics_script.py]
+    set result_python [GiD_Python_Call geomechanics_script.my_python_procedure $param1]
+    W $result_python
 }
