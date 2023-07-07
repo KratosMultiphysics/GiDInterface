32efde3fc86887559b0261df061d3ab0601ca96c
Javi Gárate <fjgarate@cimne.upc.edu>
2023-07-07 21:51:23 +0200

    prepare json

--- kratos.gid/apps/GeoMechanics/write/writeProjectParameters.tcl
+++ kratos.gid/apps/GeoMechanics/write/writeProjectParameters.tcl
@@ -16,18 +16,26 @@ proc ::GeoMechanics::write::GetSingleFileStageProjectParameters {  } {
     # Get the base dictionary for the project parameters
     set project_parameters_dict [dict create]
 
-    # Set the list of stages
+    # Set the orquestrator
     dict set project_parameters_dict orquestrator name "MultistageOrchestrators.KratosMultiphysics.SequentialMultistageOrchestrator"
     dict set project_parameters_dict orquestrator settings echo_level 0
-    dict set project_parameters_dict orquestrator settings "execution_list" [::GeoMechanics::xml::GetStages "names"]
-    dict set project_parameters_dict orquestrator "stage_checkpoints" : true
+    dict set project_parameters_dict orquestrator settings execution_list [::GeoMechanics::xml::GetStages "names"]
+    dict set project_parameters_dict orquestrator settings stage_checkpoints true
+    dict set project_parameters_dict orquestrator settings stage_checkpoints_folder new_checkpoints
+    # dict set project_parameters_dict orquestrator settings load_from_checkpoint "new_checkpoints/fluid_stage"
 
     # Set the stages
     set stages [dict create]
 
     foreach stage [::GeoMechanics::xml::GetStages] {
         set stage_name [$stage @name]
-        dict set stages $stage_name [::GeoMechanics::write::getParametersDict $stage]
+        set stage_content [::GeoMechanics::write::getParametersDict $stage]
+        dict set stage_content stage_preprocess prepare_restart write_restart true
+        dict set stage_content stage_preprocess prepare_restart restart_settings [dict create]
+        dict set stage_content stage_preprocess operations [list [dict create name "user_operation.UserOperation" parameters [dict create ] ]] 
+        dict set stage_content stage_preprocess operations [list [dict create name "user_operation.UserOperation" parameters [dict create ] ]] 
+        dict set stage_content stage_preprocess modelers [list [dict create name "KratosMultiphysics.modelers.import_mdpa_modeler.ImportMDPAModeler" Parameters [dict create ] ]]
+        dict set stages $stage_name $stage_content
     }
 
     dict set project_parameters_dict "stages" $stages
