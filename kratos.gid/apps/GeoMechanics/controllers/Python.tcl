f483c09f83ff167cc3c35c32bfd862a428588386
Javi Gárate <fjgarate@cimne.upc.edu>
2023-08-16 11:10:07 +0200

    Safety on directory read

--- kratos.gid/apps/GeoMechanics/controllers/Python.tcl
+++ kratos.gid/apps/GeoMechanics/controllers/Python.tcl
@@ -7,6 +7,9 @@ proc ::GeoMechanics::PythonButton { } {
 
 proc ::GeoMechanics::PythonButtonImportPlaxis { } {
     set directory [MessageBoxGetFilename directory read [_ "Select Plaxis model to import"]]
+    if {$directory == ""} {
+        return
+    }
     GiD_Python_Source [file join $::GeoMechanics::dir controllers geomechanics_import_plaxis.py]
     set result_python [GiD_Python_Call geomechanics_import_plaxis.import_plaxis_procedure $directory]
     W $result_python
