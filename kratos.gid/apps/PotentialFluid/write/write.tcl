188b03316eeea26aaa57150da3e6c308b0909e4d
Javi Gárate <fjgarate@cimne.upc.edu>
2023-10-18 11:08:53 +0200

    Hotfix potential init

--- kratos.gid/apps/PotentialFluid/write/write.tcl
+++ kratos.gid/apps/PotentialFluid/write/write.tcl
@@ -11,12 +11,12 @@ proc ::PotentialFluid::write::Init { } {
     variable writeAttributes
     set writeAttributes [::Fluid::write::GetAttributes]
     SetAttribute validApps [list "Fluid" "PotentialFluid"]
-    # Fluid has implemented the geometry mode, but we do not use it yet in inherited apps
-    ::Fluid::write::SetAttribute write_mdpa_mode [::PotentialFluid::GetWriteProperty write_mdpa_mode]
 }
 
 # Events
 proc PotentialFluid::write::writeModelPartEvent { } {
+    # Fluid has implemented the geometry mode, but we do not use it yet in inherited apps
+    ::Fluid::write::SetAttribute write_mdpa_mode [::PotentialFluid::GetWriteProperty write_mdpa_mode]
     # Add the PotentialFluid to the Fluid valid applications list
     Fluid::write::AddValidApps "PotentialFluid"
     Fluid::write::writeModelPartEvent
