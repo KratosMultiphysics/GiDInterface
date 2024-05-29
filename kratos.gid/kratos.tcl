71d25949e48cd83cfb561659df0c25b6aa4e805f
Javi Gárate <fjgarate@cimne.upc.edu>
2024-05-29 19:30:53 +0200

    9.4.6 version

--- kratos.gid/kratos.tcl
+++ kratos.gid/kratos.tcl
@@ -189,7 +189,7 @@ proc Kratos::InitGlobalVariables {dir} {
     set namespaces [list ]
 
     variable pip_packages_required
-    set pip_packages_required [list KratosMultiphysics-all==9.4.2]
+    set pip_packages_required [list KratosMultiphysics-all==9.4.6]
 }
 
 proc Kratos::LoadCommonScripts { } {
--- tools/create-release.bat
+++ tools/create-release.bat
@@ -1,4 +1,4 @@
-set VERSION=9.4.2
+set VERSION=9.4.6
 @REM git checkout master
 @REM git fetch -p
 @REM git pull -p
